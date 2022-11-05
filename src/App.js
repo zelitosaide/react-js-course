fedca24e7603f252eaf21e88d9d6d39b68b277e6
zelitosaide <zelitosaide83@gmail.com>
2022-11-05 11:47:33 +0200

    prettier config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 // import { MyApp } from "./learn/quick_start/MyApp";
-import {
-  FilterableProductTable
-} from "./learn/thinking_in_react/FilterableProductTable";
+import { FilterableProductTable } from "./learn/thinking_in_react/FilterableProductTable";
 
-import {
-  PRODUCTS as products
-} from "./learn/thinking_in_react/products";
-
+import { PRODUCTS as products } from "./learn/thinking_in_react/products";
 
 function App() {
   return (
     // <MyApp />
     <FilterableProductTable products={products} />
-  )
+  );
 }
 
 export default App;
--- src/learn/thinking_in_react/products.js
+++ src/learn/thinking_in_react/products.js
@@ -4,5 +4,5 @@ export const PRODUCTS = [
   { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
 ];
