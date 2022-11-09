a5de04cd41a67c39f6292d441a3b8493486c255c
zelitosaide <zelitosaide83@gmail.com>
2022-11-09 20:24:52 +0200

    README useContext api

--- src/api/use-context/multiple-contexts.js
+++ src/api/use-context/multiple-contexts.js
@@ -6,4 +6,29 @@ const CurrentUserContext = createContext(null);
 export function MultipleContexts() {
   const [theme, setTheme] = useState("light");
   const [currentUser, setCurrentUser] = useState(null);
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <CurrentUserContext.Provider
+        value={{
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        <WelcomePanel />
+        <label htmlFor="theme">Use dark mode</label>
+        <input
+          id="theme"
+          checked={theme === "dark"}
+          onChange={function (event) {
+            setTheme(event.target.checked ? "dark" : "light");
+          }}
+        />
+      </CurrentUserContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
+
+function WelcomePanel() {
+  return;
 }
