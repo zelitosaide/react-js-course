83a80889f5ac5eb78924ed1c52056fe7d59c9714
zelitosaide <zelitosaide83@gmail.com>
2022-11-09 17:53:30 +0200

    README useContext api

--- src/api/use-context/updating-a-value-via-context.js
+++ src/api/use-context/updating-a-value-via-context.js
@@ -21,5 +21,29 @@ export function UpadatingViaContext() {
 }
 
 function Form() {
-  return;
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Sign in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = "panel-" + theme;
+
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  );
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = "button-" + theme;
+
+  return <button className={className}>{children}</button>;
 }
