1ece2492d7c468751674ce75de16d8a39da9fa56
zelitosaide <zelitosaide83@gmail.com>
2022-11-10 03:52:13 +0200

    README useContext api

--- src/api/use-context/extracting-providers-to-a-component..js
+++ src/api/use-context/extracting-providers-to-a-component..js
@@ -44,4 +44,43 @@ function MyProviders({ children, theme, setTheme }) {
   );
 }
 
-function WelcomePanel() {}
+function WelcomePanel() {
+  const { currentUser } = useContext(CurrentUserContext);
+
+  return (
+    <Panel title="Welcome">
+      {currentUser !== null ? <Greeting /> : <LoginForm />}
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = "panel-" + theme;
+
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  );
+}
+
+function Greeting() {
+  const { currentUser } = useContext(CurrentUserContext);
+
+  return <p>You logged in as {currentUser.name}.</p>;
+}
+
+function LoginForm() {
+  const { setCurrentUser } = useContext(CurrentUserContext);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const canLogin = firstName !== "" && lastName !== "";
+
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}
