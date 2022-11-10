48d917d4427011606a3902babb38a11eea689360
zelitosaide <zelitosaide83@gmail.com>
2022-11-10 04:06:20 +0200

    README useContext api

--- src/api/use-context/extracting-providers-to-a-component..js
+++ src/api/use-context/extracting-providers-to-a-component..js
@@ -115,4 +115,17 @@ function LoginForm() {
   );
 }
 
-function Button({ children, disabled, onClick }) {}
+function Button({ children, disabled, onClick }) {
+  const theme = useContext(ThemeContext);
+  const className = "button-" + theme;
+
+  return (
+    <button
+      className={className}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
