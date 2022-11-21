5121fdc1e112ff52d5f762e66c87f5dfcbd1d549
zelitosaide <zelitosaide83@gmail.com>
2022-11-22 00:35:25 +0200

    memo api

---
+++ src/api/memo/react-api/wraps-other-components.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function WrapsOtherComponents() {
+  return (
+    <Wrapper>
+      <Child />
+    </Wrapper>
+  );
+}
+
+function Wrapper({ children }) {
+  const [theme, setTheme] = useState("dark");
+
+  return (
+    <div>
+      {children}
+      <p>
+        <label htmlFor="theme">Use dark mode</label>
+        <input
+          id="theme"
+          type="checkbox"
+          checked={theme === "dark"}
+          onChange={function (event) {
+            setTheme(event.target.checked ? "dark" : "light");
+          }}
+        />
+      </p>
+    </div>
+  );
+}
+
+function Child() {
+  console.log("child rendered");
+  return <div>child component</div>;
+}
