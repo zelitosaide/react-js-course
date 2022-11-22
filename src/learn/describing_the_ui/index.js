5d91424a6bcafac9f351e8248e9dfa0457aced8c
zelitosaide <zelitosaide83@gmail.com>
2022-11-22 23:07:24 +0200

    Describing the UI

--- src/learn/describing_the_ui/index.js
+++ src/learn/describing_the_ui/index.js
@@ -203,3 +203,18 @@ export function TeaSet() {
     </>
   );
 }
+
+// --------------------------------------------------------
+function Cup2({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export function TeaSet2() {
+  return (
+    <>
+      <Cup2 guest={1} />
+      <Cup2 guest={2} />
+      <Cup2 guest={3} />
+    </>
+  );
+}
