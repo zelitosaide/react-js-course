9ebc4493d2ea1f823546960e4e5b99aa83303336
zelitosaide <zelitosaide83@gmail.com>
2022-11-22 22:53:58 +0200

    Describing the UI

--- src/learn/describing_the_ui/index.js
+++ src/learn/describing_the_ui/index.js
@@ -111,3 +111,72 @@ export function PackingList() {
     </section>
   );
 }
+
+// --------------------------------------------------------
+export function List() {
+  const listItems = people.map(function (person) {
+    return (
+      <li key={person.id}>
+        <img
+          src={getImageUrl2(person)}
+          alt={person.name}
+        />
+        <p>
+          <b>{person.name}:</b>
+          {" " + person.profession + " "}
+          known for {person.accomplishment}
+        </p>
+      </li>
+    );
+  });
+
+  return (
+    <section>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </section>
+  );
+}
+
+function getImageUrl2(person) {
+  return "https://i.imgur.com/" + person.imageId + "s.jpg";
+}
+
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    imageId: "mynHUSa",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "bE7W1ji",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "IOjWm71",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    imageId: "lrWQx8l",
+  },
+];
