08b2c726471c61d6541939c374c6800176c6f054
zelitosaide <zelitosaide83@gmail.com>
2022-11-22 22:43:46 +0200

    Describing the UI

--- src/learn/describing_the_ui/index.js
+++ src/learn/describing_the_ui/index.js
@@ -94,6 +94,20 @@ export function PackingList() {
   return (
     <section>
       <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space unit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
     </section>
   );
 }
