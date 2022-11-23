f8e6638f2171d58ba2e9f6914d5c993aa63e069d
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 14:27:43 +0200

    Writing Markup with JSX

--- src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
+++ src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
@@ -60,3 +60,30 @@ export function TodoList3() {
     </ul>
   );
 }
+
+// --------------------------------------------------------
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
+
+export function TodoList4() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
