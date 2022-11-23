130eae0906132fb6b4bee6dc401f76f99695d093
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 08:55:16 +0200

    Your first component

--- src/learn/describing_the_ui/your-first-component.js
+++ src/learn/describing_the_ui/your-first-component.js
@@ -11,3 +11,35 @@ export function YourFirstComponent() {}
 </article>;
 
 // --------------------------------------------------------
+<PageLayout>
+  <NavigationHeader>
+    <SearchBar />
+    <Link to="/docs">Docs</Link>
+  </NavigationHeader>
+  <Sidebar />
+  <PageContent>
+    <TableOfContents />
+    <DocumentationText />
+  </PageContent>
+</PageLayout>;
+
+// --------------------------------------------------------
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
