f294577dc048ecb51170fa8efcc1b8bebf4edbf7
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 09:51:34 +0200

    Importing and Exporting Components

--- src/learn/describing_the_ui/importing-and-exporting-components.js
+++ src/learn/describing_the_ui/importing-and-exporting-components.js
@@ -21,3 +21,32 @@ export default function Gallery() {
     </section>
   );
 }
+
+// --------------------------------------------------------
+// app.js
+// import { Gallery2 } from "./Gallery2.js";
+
+export function App() {
+  return <Gallery2 />;
+}
+
+// gallery2.js
+export function Profile2() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
+
+export function Gallery2() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
