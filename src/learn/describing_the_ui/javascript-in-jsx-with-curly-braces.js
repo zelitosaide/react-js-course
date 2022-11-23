4c1438b7473cdffb48c7e87e588d8b84c65630dc
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 14:04:14 +0200

    Writing Markup with JSX

--- src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
+++ src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
@@ -1,1 +1,26 @@
 export function JavaScriptJSXCurlyBraces() {}
+
+// --------------------------------------------------------
+export function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/7vQD0fPs.jpg"
+      alt="Gregorio Y. Zara"
+    />
+  );
+}
+
+// --------------------------------------------------------
+export function Avatar2() {
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+
+  return (
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
+}
