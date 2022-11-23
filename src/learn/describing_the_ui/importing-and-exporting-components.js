f82a03fdd87be642fa14657cf60dc46c51a825cd
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 09:32:10 +0200

    Importing and Exporting Components

---
+++ src/learn/describing_the_ui/importing-and-exporting-components.js
@@ -0,0 +1,23 @@
+export function ImportingExportingComponents() {}
+
+// --------------------------------------------------------
+// These currently lives in a root component file
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
