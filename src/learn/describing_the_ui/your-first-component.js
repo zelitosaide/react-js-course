1f55956ac41a594f2fceac673bf52541b6e0ff0f
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 08:39:18 +0200

    Your first component

--- src/learn/describing_the_ui/your-first-component.js
+++ src/learn/describing_the_ui/your-first-component.js
@@ -1,1 +1,13 @@
 export function YourFirstComponent() {}
+
+// --------------------------------------------------------
+<article>
+  <h1>My First Component</h1>
+  <ol>
+    <li>Components: UI Building Blocks</li>
+    <li>Defining a Component</li>
+    <li>Using a Component</li>
+  </ol>
+</article>;
+
+// --------------------------------------------------------
