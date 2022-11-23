717d9cf6bce74ed4824d364ed37b4582cfb6e745
zelitosaide <zelitosaide83@gmail.com>
2022-11-23 14:07:48 +0200

    Writing Markup with JSX

--- src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
+++ src/learn/describing_the_ui/javascript-in-jsx-with-curly-braces.js
@@ -24,3 +24,11 @@ export function Avatar2() {
     />
   );
 }
+
+// --------------------------------------------------------
+export function TodoList() {
+  const name = "Gregorio Y. Zara";
+  return <h1>{name}'s To Do List</h1>;
+}
+
+// --------------------------------------------------------
