7039fca5214ff4d46559b062f12608234083d3a6
zelitosaide <zelitosaide83@gmail.com>
2023-01-01 17:54:14 +0200

    Get started

--- src/learn/get-started/quick-start/index.js
+++ src/learn/get-started/quick-start/index.js
@@ -87,3 +87,5 @@ function LoginForm() {
     </div>
   );
 }
+
+// Rendering lists
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,3 +3,9 @@ export const user = {
   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
   imageSize: 90,
 };
+
+export const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
