15719dbe3201de36fcbe69a25be904a62ae38753
zelitosaide <zelitosaide83@gmail.com>
2023-01-02 11:23:23 +0200

    Get started

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -7,40 +7,29 @@ export default function TicTacToe() {
 }
 
 export function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
 }
 
-export function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+export function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
