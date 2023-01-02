ffe74f7100bf84aea71ee192ee68f3919b8fd03a
zelitosaide <zelitosaide83@gmail.com>
2023-01-02 10:42:45 +0200

    Tic Tac Toe

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -7,15 +7,21 @@ export default function TicTacToe() {
 export function Square() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </>
   );
 }
