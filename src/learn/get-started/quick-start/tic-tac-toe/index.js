ff14b349a9081268660d18453cba1dace4681d96
zelitosaide <zelitosaide83@gmail.com>
2023-01-02 12:59:08 +0200

    Get started

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -104,14 +104,14 @@ export function Square({ value, onSquareClick }) {
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2], //
+    [3, 4, 5], //
+    [6, 7, 8], //
+    [0, 3, 6], //
+    [1, 4, 7], //
+    [2, 5, 8], //
+    [0, 4, 8], //
+    [2, 4, 6], //
   ];
 
   for (let i = 0; i < lines.length; i++) {
