c20236d2714a8b33367e5ba3756dc53bc4d971cb
zelitosaide <zelitosaide83@gmail.com>
2023-01-02 11:39:47 +0200

    Get started

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -9,10 +9,19 @@ export default function TicTacToe() {
 export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square
+          value={squares[0]}
+          onSquareClick={handleClick}
+        />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
