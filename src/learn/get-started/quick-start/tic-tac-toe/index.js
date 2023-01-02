356edc1ac9f6714f98713ca25d9734e1c9946960
zelitosaide <zelitosaide83@gmail.com>
2023-01-02 10:58:21 +0200

    Tic Tac Toe

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -8,24 +8,24 @@ export function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
 }
 
-export function Square() {
-  return <button className="square">1</button>;
+export function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
