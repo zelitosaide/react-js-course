25c97dd80df8562a0d238a7e576d7d1cf38c9caa
zelitosaide <zelitosaide83@gmail.com>
2023-01-03 13:38:23 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -3,26 +3,13 @@ import { useState } from "react";
 import "./style.css";
 
 export default function TicTacToe() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
-  const currentSquares = history[history.length - 1];
-
-  function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
-  }
-
   return (
     <div className="game">
       <div className="game-board">
-        <Board
-          xIsNext={xIsNext}
-          squares={currentSquares}
-          onPlay={handlePlay}
-        />
+        <Board />
       </div>
       <div className="game-info">
-        <ol></ol>
+        <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
