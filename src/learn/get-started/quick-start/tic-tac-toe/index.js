ab86c51b41fc89ff15eab813bf5d659dac1df729
zelitosaide <zelitosaide83@gmail.com>
2023-01-03 13:46:05 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "./style.css";
 
 export default function TicTacToe() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
   return (
     <div className="game">
       <div className="game-board">
