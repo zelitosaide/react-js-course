88ec7ef7ca5b380e561f29ed6703ea2414e72dda
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 14:14:50 +0200

    TIC TAC TOE

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 //   UpdatingTheScreen,
 // } from "./learn/get-started/quick-start";
 
-import Square from "./learn/get-started/quick-start/tic-tac-toe";
+import TicTacToeGame from "./learn/get-started/quick-start/tic-tac-toe";
 
 function App() {
   return (
@@ -27,7 +27,7 @@ function App() {
 
       {/* Tic Tac Toe */}
       {/* <TicTacToe /> */}
-      <Square />
+      <TicTacToeGame />
     </>
   );
 }
