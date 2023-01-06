ad42ad5eb6868af664caf97245f47d2b6735e0d1
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 06:52:45 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -1,5 +1,10 @@
 import "./style.css";
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
