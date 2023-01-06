1545f283781c59b96a7461eb4fc77d47e246fb4f
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 06:57:50 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -4,19 +4,19 @@ export default function Square() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
       </div>
     </>
   );
