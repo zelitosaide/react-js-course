5697519f7f25b13e3991edec0566d69fd8259058
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 10:32:26 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -4,32 +4,61 @@ import "./style.css";
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick() {}
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square
+          value={squares[0]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={handleClick}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square
+          value={squares[3]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={handleClick}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square
+          value={squares[6]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={handleClick}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={handleClick}
+        />
       </div>
     </>
   );
 }
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="square"
-      // onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
