677713f63e4edb120ed80c4fb9a47689b3b78cd9
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 19:37:04 +0200

    Describing the UI

--- src/utils/data.js
+++ src/utils/data.js
@@ -9,3 +9,12 @@ export const products = [
   { title: "Garlic", isFruit: false, id: 2 },
   { title: "Apple", isFruit: true, id: 3 },
 ];
+
+export const products2 = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
