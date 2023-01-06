232c8042961fd77e74ae90a61d9f7e16dbde31ad
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 06:56:17 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -3,8 +3,21 @@ import "./style.css";
 export default function Square() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
     </>
   );
 }
