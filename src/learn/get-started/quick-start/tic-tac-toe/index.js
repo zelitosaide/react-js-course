9beeafe4ac6db271b2dabb47625c8973d8a3630f
zelitosaide <zelitosaide83@gmail.com>
2023-01-06 18:25:15 +0200

    TIC TAC TOE

--- src/learn/get-started/quick-start/tic-tac-toe/index.js
+++ src/learn/get-started/quick-start/tic-tac-toe/index.js
@@ -180,4 +180,11 @@ function calculateWinner(squares) {
  * If you have extra time or want to practice your new React skills, here are some ideas
  * for improvements that you could make to the tic-tac-toe game, listed in order of
  * increasing difficulty:
+ *
+ * 1. For the current move only, show “You are at move #…” instead of a button
+ * 2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+ * 3. Add a toggle button that lets you sort the moves in either ascending or descending order.
+ * 4. When someone wins, highlight the three squares that caused the win (and when no one wins,
+ *    display a message about the result being a draw).
+ * 5. Display the location for each move in the format (col, row) in the move history list.
  */
