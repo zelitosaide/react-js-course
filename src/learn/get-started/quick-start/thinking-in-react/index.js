43d34419fdaa5e837a751d466b7fb34b6fd93115
zelitosaide <zelitosaide83@gmail.com>
2023-01-19 22:04:27 +0200

    Thinking in React

--- src/learn/get-started/quick-start/thinking-in-react/index.js
+++ src/learn/get-started/quick-start/thinking-in-react/index.js
@@ -1,4 +1,11 @@
-export default function FilterableProductTable() {}
+export default function FilterableProductTable() {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable />
+    </div>
+  );
+}
 
 function SearchBar() {}
 
@@ -10,4 +17,6 @@ function ProductRow() {}
 
 /**
  * Step 1: Break the UI into a component hierarchy
+ *
+ * Step 2: Build a static version in React
  */
