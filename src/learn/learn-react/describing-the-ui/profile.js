dc34bc17efce11d0e3189199b05217f19efd536e
zelitosaide <zelitosaide83@gmail.com>
2023-01-20 20:33:59 +0200

    Describing the UI

---
+++ src/learn/learn-react/describing-the-ui/profile.js
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+      style={{ margin: "0 10px 10px 0" }}
+    />
+  );
+}
