36290fbd1dae21e04340cd51af313531b265a9a3
zelitosaide <zelitosaide83@gmail.com>
2023-01-20 22:59:36 +0200

    Describing the UI

---
+++ src/learn/learn-react/describing-the-ui/tea-set2.js
@@ -0,0 +1,13 @@
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
