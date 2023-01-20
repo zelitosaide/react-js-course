4f7b2d86a25f9331b2e25566e3b668191ba0208a
zelitosaide <zelitosaide83@gmail.com>
2023-01-20 09:03:22 +0200

    Thinking in React

--- src/learn/get-started/quick-start/thinking-in-react/index.js
+++ src/learn/get-started/quick-start/thinking-in-react/index.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export default function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
     </div>
   );
 }
