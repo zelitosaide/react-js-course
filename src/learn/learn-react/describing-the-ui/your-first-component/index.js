67470f48866aa7e2f1ceac5200cefa8f5d1c8d74
zelitosaide <zelitosaide83@gmail.com>
2023-01-21 13:32:36 +0200

    Your first component

---
+++ src/learn/learn-react/describing-the-ui/your-first-component/index.js
@@ -0,0 +1,12 @@
+export default function Index() {
+  return <Profile />;
+}
+
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
