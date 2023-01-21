fa1e76922053b93b6acf55e81d5e395e90c8017c
zelitosaide <zelitosaide83@gmail.com>
2023-01-21 13:43:15 +0200

    Your first component

--- src/learn/learn-react/describing-the-ui/your-first-component/index.js
+++ src/learn/learn-react/describing-the-ui/your-first-component/index.js
@@ -1,5 +1,5 @@
 export default function Index() {
-  return <Profile />;
+  return <Gallery />;
 }
 
 function Profile() {
@@ -7,6 +7,26 @@ function Profile() {
     <img
       src="https://i.imgur.com/MK3eW3Am.jpg"
       alt="Katherine Johnson"
+      style={{ height: 200 }}
     />
   );
 }
+
+function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+/**
+ * Step 1: Export the component
+ *
+ * Step 2: Define the function
+ *
+ * Step 3: Add markup
+ */
