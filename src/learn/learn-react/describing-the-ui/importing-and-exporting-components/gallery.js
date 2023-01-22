ff3ee795d05711c3c43e375ac7c23f2f925a8df0
zelitosaide <zelitosaide83@gmail.com>
2023-01-22 07:54:40 +0200

    Importing and Exporting Components

--- src/learn/learn-react/describing-the-ui/importing-and-exporting-components/gallery.js
+++ src/learn/learn-react/describing-the-ui/importing-and-exporting-components/gallery.js
@@ -7,4 +7,13 @@ function Profile() {
   );
 }
 
-export default function Gallery() {}
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
