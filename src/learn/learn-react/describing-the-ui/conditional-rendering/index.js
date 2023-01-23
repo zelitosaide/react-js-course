750f64be9d79a05cd86f51b1fefe02b1d324ab5d
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 16:14:14 +0200

    Conditional Rendering

--- src/learn/learn-react/describing-the-ui/conditional-rendering/index.js
+++ src/learn/learn-react/describing-the-ui/conditional-rendering/index.js
@@ -3,3 +3,11 @@ export default function Index() {}
 function Item({ name, isPacked }) {
   return <li className="item">{name}</li>;
 }
+
+function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+    </section>
+  );
+}
