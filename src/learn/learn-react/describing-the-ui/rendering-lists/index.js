0900f07fe6c9b0387cd29fdb28a6da4501298db3
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 19:52:40 +0200

    Rendering Lists

--- src/learn/learn-react/describing-the-ui/rendering-lists/index.js
+++ src/learn/learn-react/describing-the-ui/rendering-lists/index.js
@@ -1,1 +1,13 @@
-export default function Index() {}
+import { people } from "../../../../utils/data";
+
+export default function Index() {
+  return <List />;
+}
+
+function List() {
+  const listItems = people.map(function (person) {
+    return <li key={person.id}>{person.name}</li>;
+  });
+
+  return <ul>{listItems}</ul>;
+}
