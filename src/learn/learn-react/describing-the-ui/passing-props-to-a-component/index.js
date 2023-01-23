d7454dde195ce964d1ee95777a723cdcae5d718f
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 11:57:30 +0200

    Passing Props to a Component

--- src/learn/learn-react/describing-the-ui/passing-props-to-a-component/index.js
+++ src/learn/learn-react/describing-the-ui/passing-props-to-a-component/index.js
@@ -1,1 +1,13 @@
 export default function Index() {}
+
+function Avatar() {
+  return (
+    <img
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+      className="avatar"
+    />
+  );
+}
