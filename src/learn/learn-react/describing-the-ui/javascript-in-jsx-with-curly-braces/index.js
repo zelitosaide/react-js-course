fcbe29b576a7310c792b50b4d8b02330e2c956f0
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 10:06:20 +0200

    JavaScript in JSX with Curly Braces

--- src/learn/learn-react/describing-the-ui/javascript-in-jsx-with-curly-braces/index.js
+++ src/learn/learn-react/describing-the-ui/javascript-in-jsx-with-curly-braces/index.js
@@ -1,1 +1,13 @@
-export default function Index() {}
+export default function Index() {
+  return <Avatar />;
+}
+
+function Avatar() {
+  return (
+    <img
+      src="https://i.imgur.com/7vQD0fPs.jpg"
+      alt="Gregorio Y. Zara"
+      className="avatar"
+    />
+  );
+}
