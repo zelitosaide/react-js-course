658223e1de54c99379297dc8da7b8f0ec04ea8f1
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 16:16:12 +0200

    Conditional Rendering

--- src/learn/learn-react/describing-the-ui/conditional-rendering/index.js
+++ src/learn/learn-react/describing-the-ui/conditional-rendering/index.js
@@ -8,6 +8,20 @@ function PackingList() {
   return (
     <section>
       <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          name="Space suit"
+          isPacked={true}
+        />
+        <Item
+          name="Helmet with a golden leaf"
+          isPacked={true}
+        />
+        <Item
+          name="Photo of Tam"
+          isPacked={false}
+        />
+      </ul>
     </section>
   );
 }
