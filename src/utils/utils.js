7d8d87f4913f945672ce9b8c8e1051ec783b2e00
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 20:19:51 +0200

    Rendering Lists

---
+++ src/utils/utils.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function getImageUrl(person, size = "s") {
+  return `https://i.imgur.com/${person.imageId + size}.jpg`;
+}
+
+export function formatDate(date) {
+  // return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+  return new Intl.DateTimeFormat("pt-PT", { weekday: "long" }).format(date);
+}
+
+export function useTime() {
+  const [time, setTime] = useState(function () {
+    return new Date();
+  });
+
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(new Date());
+    }, 1000);
+
+    return function () {
+      clearInterval(id);
+    };
+  }, []);
+
+  return time;
+}
