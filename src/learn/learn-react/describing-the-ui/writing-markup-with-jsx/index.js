2579020210dd908539690ca09d6e915d40433315
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 09:45:38 +0200

    Writing Markup with JSX

--- src/learn/learn-react/describing-the-ui/writing-markup-with-jsx/index.js
+++ src/learn/learn-react/describing-the-ui/writing-markup-with-jsx/index.js
@@ -1,4 +1,20 @@
-export default function Index() {}
+export default function Index() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
 
 /**
  * The Rules of JSX
@@ -6,4 +22,8 @@ export default function Index() {}
  * 1. Return a single root element
  *
  * 2. Close all the tags
+ *
+ * 3. camelCase all most of the things!
+ *
+ *    For historical reasons, aria-* and data-* attributes are written as in HTML with dashes.
  */
