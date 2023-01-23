45ecef79e48ec9f26c88b1c1cbcac661436499e2
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 20:03:56 +0200

    Rendering Lists

--- src/learn/learn-react/describing-the-ui/rendering-lists/index.js
+++ src/learn/learn-react/describing-the-ui/rendering-lists/index.js
@@ -5,6 +5,18 @@ export default function Index() {
 }
 
 function List() {
+  const chemists = people.filter(function (person) {
+    return person.profession === "chemist";
+  });
+
+  const listItems = chemists.map(function (person) {
+    return <li key={person.id}>{person.name}</li>;
+  });
+
+  return <ul>{listItems}</ul>;
+}
+
+function List2() {
   const listItems = people.map(function (person) {
     return <li key={person.id}>{person.name}</li>;
   });
