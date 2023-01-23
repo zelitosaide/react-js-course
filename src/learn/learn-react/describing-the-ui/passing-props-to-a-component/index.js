b2e503b7c76ee4d621974887e2dd6f2f49d7806f
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 12:13:51 +0200

    Passing Props to a Component

--- src/learn/learn-react/describing-the-ui/passing-props-to-a-component/index.js
+++ src/learn/learn-react/describing-the-ui/passing-props-to-a-component/index.js
@@ -1,5 +1,16 @@
+import { getImageUrl } from "../../../../utils/services";
+
 export default function Index() {
   return <Avatar />;
+}
+
+function Profile() {
+  return (
+    <Avatar2
+      person={{ name: "Lin Lanying", imageId: "1bX5QH6" }}
+      size={100}
+    />
+  );
 }
 
 function Avatar() {
@@ -10,6 +21,24 @@ function Avatar() {
       width={100}
       height={100}
       className="avatar"
+      style={{ width: 100, height: 100 }}
     />
   );
 }
+
+function Avatar2({ person, size }) {
+  return (
+    <img
+      src={getImageUrl(person)}
+      alt={person.name}
+      style={{ width: size, height: size }}
+      className="avatar"
+    />
+  );
+}
+
+/**
+ * Step 1: Pass props to the child component
+ *
+ * Step 2: Read props inside the child component
+ */
