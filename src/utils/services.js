2bb061c5dc20f05228c13a817748f1f99fc706ae
zelitosaide <zelitosaide83@gmail.com>
2023-01-23 10:30:21 +0200

    JavaScript in JSX with Curly Braces

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,3 +1,7 @@
 export function getImageUrl(person, size = "s") {
   return `https://i.imgur.com/${person.imageId + size}.jpg`;
 }
+
+export function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
