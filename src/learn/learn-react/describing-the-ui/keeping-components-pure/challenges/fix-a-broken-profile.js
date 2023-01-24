81bcf0523bb0c5b34a885f039167f3781c81d008
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 17:01:08 +0200

    Keeping Components Pure

---
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/challenges/fix-a-broken-profile.js
@@ -0,0 +1,21 @@
+/**
+ * Fix a broken profile
+ *
+ * Two Profile components are rendered side by side with different
+ * data. Press “Collapse” on the first profile, and then “Expand”
+ * it. You’ll notice that both profiles now show the same person. This is a bug.
+ *
+ * Find the cause of the bug and fix it.
+ */
+
+import { useState } from "react";
+
+function Profile({ person }) {
+  return <Panel></Panel>;
+}
+
+function Panel({ children }) {
+  const [open, setOpen] = useState(true);
+
+  return <section className="panel"></section>;
+}
