f3a011422d96358dc703bd91b6f6995bff63dec1
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 16:38:17 +0200

    Keeping Components Pure

---
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/challenges/fix-a-broken-clock.js
@@ -0,0 +1,12 @@
+/**
+ * Fix a broken clock
+ *
+ * This component tries to set the <h1>’s CSS class to "night" during the
+ * time from midnight to six hours in the morning, and "day" at all other
+ * times. However, it doesn’t work. Can you fix this component?
+ *
+ * You can verify whether your solution works by temporarily changing the
+ * computer’s timezone. When the current time is between midnight and six
+ * in the morning, the clock should have inverted colors!
+ */
+export default function Clock() {}
