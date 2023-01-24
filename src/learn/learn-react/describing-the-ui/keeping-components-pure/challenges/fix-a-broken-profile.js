e0bcbf88db8711662299cf655a232efb935db566
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 17:09:09 +0200

    Keeping Components Pure

--- src/learn/learn-react/describing-the-ui/keeping-components-pure/challenges/fix-a-broken-profile.js
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/challenges/fix-a-broken-profile.js
@@ -9,9 +9,30 @@
  */
 
 import { useState } from "react";
+import { getImageUrl } from "../../../../../utils/services";
+
+let currentPerson;
 
 function Profile({ person }) {
+  currentPerson = person;
+
   return <Panel></Panel>;
+}
+
+function Header() {
+  return <h1>{currentPerson.name}</h1>;
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(currentPerson)}
+      alt={currentPerson.name}
+      width={50}
+      height={50}
+    />
+  );
 }
 
 function Panel({ children }) {
