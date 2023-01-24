51b8b5276c0830c3f9b0a11e2aab81756cec386a
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 17:49:16 +0200

    Keeping Components Pure

---
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/challenges/fix-a-broken-story-tray.js
@@ -0,0 +1,12 @@
+/**
+ * Fix a broken story tray
+ *
+ * The CEO of your company is asking you to add “stories” to your online clock
+ * app, and you can’t say no. You’ve written a StoryTray component that accepts a
+ * list of stories, followed by a “Create Story” placeholder.
+ *
+ * You implemented the “Create Story” placeholder by pushing one more fake story
+ * at the end of the stories array that you receive as a prop. But for some reason,
+ * “Create Story” appears more than once. Fix the issue.
+ */
+export default function Index() {}
