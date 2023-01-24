cf4421b28a5c33f723356a5f699dee7478ddc5f1
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 13:52:29 +0200

    Keeping Components Pure

--- src/learn/learn-react/describing-the-ui/keeping-components-pure/index.js
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/index.js
@@ -38,3 +38,11 @@ function Recipes() {
     </section>
   );
 }
+
+/**
+ * You will learn
+ *
+ * What purity is and how it helps you avoid bugs
+ *
+ * How to keep components pure by keeping changes out of the render phase
+ */
