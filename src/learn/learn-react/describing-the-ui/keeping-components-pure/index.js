4cee9a468b7e0b94918032de6173d9c1f77393af
zelitosaide <zelitosaide83@gmail.com>
2023-01-24 08:21:18 +0200

    Keeping Components Pure

--- src/learn/learn-react/describing-the-ui/keeping-components-pure/index.js
+++ src/learn/learn-react/describing-the-ui/keeping-components-pure/index.js
@@ -4,6 +4,9 @@ function Recipe({ drinkers }) {
   return (
     <ol>
       <li>Boil {drinkers} cups of water.</li>
+      <li>
+        Add {drinkers} spoons of tea and {0.5 * drinkers} of spoons of spice.
+      </li>
     </ol>
   );
 }
