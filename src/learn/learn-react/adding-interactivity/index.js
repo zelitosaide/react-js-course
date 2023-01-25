994f30026d61318f18445ec550432c89aea50d89
zelitosaide <zelitosaide83@gmail.com>
2023-01-25 12:15:53 +0200

    Adding Interactivity

--- src/learn/learn-react/adding-interactivity/index.js
+++ src/learn/learn-react/adding-interactivity/index.js
@@ -2,7 +2,27 @@ export default function Index() {
   return <RespondingToEvents />;
 }
 
-function RespondingToEvents() {}
+function RespondingToEvents() {
+  return (
+    <Toolbar
+      onPlayMovie={function () {
+        alert("Playing!");
+      }}
+      onUploadImage={function () {
+        alert("Uploading!");
+      }}
+    />
+  );
+}
+
+function Toolbar({ onPlayMovie, onUploadImage }) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onUploadImage}>Upload Image</Button>
+    </div>
+  );
+}
 
 function Button({ onClick, children }) {
   return <button onClick={onClick}>{children}</button>;
