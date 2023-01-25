588393f6af735ec21d64dd3a32b3c23e51044d0a
zelitosaide <zelitosaide83@gmail.com>
2023-01-25 18:18:19 +0200

    Adding Interactivity

--- src/learn/learn-react/adding-interactivity/index.js
+++ src/learn/learn-react/adding-interactivity/index.js
@@ -15,7 +15,7 @@ function Gallery() {
   const [showMore, setShowMore] = useState(false);
 
   function handleNextClick() {
-    if (index < sculptureList.length) {
+    if (index < sculptureList.length - 1) {
       setIndex(index + 1);
     }
   }
