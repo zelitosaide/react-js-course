d96c347c2aaffb485814b9ac1dbc2cf61244c098
zelitosaide <zelitosaide83@gmail.com>
2023-01-25 17:59:04 +0200

    Adding Interactivity

--- src/learn/learn-react/adding-interactivity/index.js
+++ src/learn/learn-react/adding-interactivity/index.js
@@ -12,6 +12,7 @@ export default function Index() {
 
 function Gallery() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
   function handleNextClick() {
     setIndex(index + 1);
@@ -28,6 +29,7 @@ function Gallery() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button></button>
     </>
   );
 }
