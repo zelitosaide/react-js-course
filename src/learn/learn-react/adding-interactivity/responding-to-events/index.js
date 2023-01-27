6448739548d52a396a833a007f25e8ec8a47e1d2
zelitosaide <zelitosaide83@gmail.com>
2023-01-27 10:45:57 +0200

    Responding to Events

--- src/learn/learn-react/adding-interactivity/responding-to-events/index.js
+++ src/learn/learn-react/adding-interactivity/responding-to-events/index.js
@@ -3,5 +3,9 @@ export default function Index() {
 }
 
 function Button() {
-  return <button>I don't do anything</button>;
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  return <button onClick={handleClick}>I don't do anything</button>;
 }
