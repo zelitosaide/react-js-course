03c70d14a1bdab7f6bf3270e4a744867197edd32
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 15:15:31 +0200

    State: A Component's Memory

--- src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/fix-stuck-form-inputs.js
+++ src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/fix-stuck-form-inputs.js
@@ -1,3 +1,22 @@
 import { useState } from "react";
 
 export default function Index() {}
+
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {
+    console.log(e.target);
+  }
+
+  return (
+    <form>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+    </form>
+  );
+}
