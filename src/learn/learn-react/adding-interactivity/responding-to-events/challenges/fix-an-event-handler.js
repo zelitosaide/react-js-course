8c503169d2dd4cc15517611d862572f34bea6619
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 09:15:55 +0200

    Responding to events

--- src/learn/learn-react/adding-interactivity/responding-to-events/challenges/fix-an-event-handler.js
+++ src/learn/learn-react/adding-interactivity/responding-to-events/challenges/fix-an-event-handler.js
@@ -9,14 +9,3 @@ export default function LightSwitch() {
   }
   return <button onClick={handleClick}>Toggle the light</button>;
 }
-export default function LightSwitch() {
-  function handleClick() {
-    const bodyStyle = document.body.style;
-    if (bodyStyle.background === "black") {
-      bodyStyle.background = "white";
-    } else {
-      bodyStyle.background = "black";
-    }
-  }
-  return <button onClick={handleClick}>Toggle the light</button>;
-}
