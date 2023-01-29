f6b130c896b6dab235214ce490eb55f4455b61c5
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 18:49:01 +0200

    State: A Component's Memory

--- src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/fix-stuck-form-inputs.js
+++ src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/fix-stuck-form-inputs.js
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+/**
+ *
+ * Challenge 2 of 4: Fix stuck form inputs
+ *
+ * When you type into the input fields, nothing appears. It’s like
+ * the input values are “stuck” with empty strings. The value of
+ * the first <input> is set to always match the firstName variable,
+ * and the value for the second <input> is set to always match the
+ * lastName variable. This is correct. Both inputs have onChange
+ * event handlers, which try to update the variables based on the
+ * latest user input (e.target.value). However, the variables don’t
+ * seem to “remember” their values between re-renders. Fix this by
+ * using state variables instead.
+ */
 export default function Index() {
   return <Form />;
 }
