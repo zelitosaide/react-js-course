e54a2bc12dfe1c8438b2dd2ad5e7538e9c499699
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 09:21:54 +0200

    Responding to events

--- src/learn/learn-react/adding-interactivity/responding-to-events/challenges/wire-up-the-events.js
+++ src/learn/learn-react/adding-interactivity/responding-to-events/challenges/wire-up-the-events.js
@@ -2,4 +2,17 @@ import { useState } from "react";
 
 export default function Index() {
   const [clicks, setClicks] = useState(0);
+
+  function handleClickOutside() {
+    setClicks(function (c) {
+      return c + 1;
+    });
+  }
+
+  return (
+    <div
+      style={{ width: "100%", height: "100%" }}
+      onClick={handleClickOutside}
+    ></div>
+  );
 }
