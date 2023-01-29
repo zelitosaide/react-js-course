caa64dcc29ac075805b716da7e142f693d1a261f
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 13:23:11 +0200

    State: A Component's Memory

---
+++ src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/complete-the-gallery.js
@@ -0,0 +1,12 @@
+/**
+ * Challenge 1 of 4: Complete the gallery
+ *
+ * When you press “Next” on the last sculpture, the code crashes. Fix
+ * the logic to prevent the crash. You may do this by adding extra
+ * logic to event handler or by disabling the button when the action
+ * is not possible.
+ *
+ * After fixing the crash, add a “Previous” button that shows the
+ * previous sculpture. It shouldn’t crash on the first sculpture.
+ */
+export default function Index() {}
