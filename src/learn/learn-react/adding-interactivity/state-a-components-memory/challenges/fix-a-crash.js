a8f7129df6c268f0a4ae738b1d266d33f9b516e8
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 18:51:40 +0200

    State: A Component's Memory

---
+++ src/learn/learn-react/adding-interactivity/state-a-components-memory/challenges/fix-a-crash.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+/**
+ * Challenge 3 of 4: Fix a crash
+ *
+ * Here is a small form that is supposed to let the user leave some
+ * feedback. When the feedback is submitted, it’s supposed to display
+ * a thank-you message. However, it crashes with an error message
+ * saying “Rendered fewer hooks than expected”. Can you spot the mistake and fix it?
+ */
+
+export default function Index() {}
