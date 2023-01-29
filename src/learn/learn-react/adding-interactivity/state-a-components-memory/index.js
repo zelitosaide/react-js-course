6df474e75f140bd00e2d90762c4a4e126c0d3c8e
zelitosaide <zelitosaide83@gmail.com>
2023-01-29 10:51:22 +0200

    Responding to events

--- src/learn/learn-react/adding-interactivity/state-a-components-memory/index.js
+++ src/learn/learn-react/adding-interactivity/state-a-components-memory/index.js
@@ -1,3 +1,11 @@
 import { sculptureList } from "../../../../data/data";
 
-export default function Index() {}
+export default function Index() {
+  function handleClick() {}
+
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+    </>
+  );
+}
