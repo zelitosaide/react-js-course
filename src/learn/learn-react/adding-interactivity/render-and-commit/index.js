25e7b0befdb466c9286fad0d32d6400750f9cb16
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 07:10:02 +0200

    Render and commit

--- src/learn/learn-react/adding-interactivity/render-and-commit/index.js
+++ src/learn/learn-react/adding-interactivity/render-and-commit/index.js
@@ -1,1 +1,12 @@
-export default function Index() {}
+export default function Index() {
+  return <Image />;
+}
+
+function Image() {
+  return (
+    <img
+      src="https://i.imgur.com/ZF6s192.jpg"
+      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  );
+}
