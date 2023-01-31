22c0b9dee1a06c6ff51fdc5a74505d2e974c6666
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 12:02:00 +0200

    State as a Snapshot

--- src/learn/learn-react/adding-interactivity/state-as-a-snapshot/challenges/implement-a-traffic-light.js
+++ src/learn/learn-react/adding-interactivity/state-as-a-snapshot/challenges/implement-a-traffic-light.js
@@ -1,1 +1,17 @@
+import { useState } from "react";
+
 export default function Index() {}
+
+function TrafficLight() {
+  const [walk, setWalk] = useState(false);
+
+  function handleClick() {
+    setWalk(!walk);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}></button>
+    </>
+  );
+}
