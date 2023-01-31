cc43b45b4da8237f6ac20017f5e31de8c27c4cb8
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 17:00:26 +0200

    Queueing a Series of State Updates

--- src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/challenges/fix-a-request-counter.js
+++ src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/challenges/fix-a-request-counter.js
@@ -7,4 +7,26 @@ export default function Index() {
   return <RequestTracker />;
 }
 
-function RequestTracker() {}
+function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending(pending + 1);
+    await delay(3000);
+  }
+
+  return (
+    <>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
+    </>
+  );
+}
+
+function delay(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, ms);
+  });
+}
