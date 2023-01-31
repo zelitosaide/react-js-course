39ab991ca038bf58451e7844d78eae4dfc71f9bb
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 17:33:14 +0200

    Queueing a Series of State Updates

--- src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/challenges/fix-a-request-counter.js
+++ src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/challenges/fix-a-request-counter.js
@@ -1,5 +1,14 @@
 /**
  * Challenge 1 of 2: Fix a request counter
+ *
+ * You’re working on an art marketplace app that lets the user submit multiple orders
+ * for an art item at the same time. Each time the user presses the “Buy” button, the
+ * “Pending” counter should increase by one. After three seconds, the “Pending” counter
+ * should decrease, and the “Completed” counter should increase.
+ *
+ * However, the “Pending” counter does not behave as intended. When you press “Buy”, it
+ * decreases to -1 (which should not be possible!). And if you click fast twice, both
+ * counters seem to behave unpredictably.
  */
 import { useState } from "react";
 
