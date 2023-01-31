feacb166b44524d1cbe2783187add8f81d1277cd
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 13:16:54 +0200

    Queueing a Series of State Updates

--- src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/index.js
+++ src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/index.js
@@ -1,1 +1,23 @@
-export default function Index() {}
+import { useState } from "react";
+
+export default function Index() {
+  const [number, setNumber] = useState(0);
+  const [isSent, setIsSent] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={function () {
+          setNumber(number + 1);
+          setTimeout(function () {
+            setIsSent(!isSent);
+          }, 5000);
+        }}
+      >
+        Click me
+      </button>
+      <p>{number}</p>
+      <p>{isSent && "Ola"}</p>
+    </>
+  );
+}
