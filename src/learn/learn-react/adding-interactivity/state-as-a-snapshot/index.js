2fda3a51c901ca7672adf553ff2104689b61c022
zelitosaide <zelitosaide83@gmail.com>
2023-01-31 10:01:39 +0200

    State as a Snapshot

--- src/learn/learn-react/adding-interactivity/state-as-a-snapshot/index.js
+++ src/learn/learn-react/adding-interactivity/state-as-a-snapshot/index.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function Index() {}
 
 function Form() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("");
+
   return (
     <form>
-      <textarea placeholder="Message" />
+      <textarea
+        value={message}
+        onChange={function (e) {
+          setMessage(e.target.value);
+        }}
+        placeholder="Message"
+      />
+      <button type="submit">Send</button>
     </form>
   );
 }
+
+function sendMessage() {}
