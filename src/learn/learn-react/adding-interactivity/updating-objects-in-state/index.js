014dab4e6517553b8871ba8488d770f1c4abce48
zelitosaide <zelitosaide83@gmail.com>
2023-02-01 15:45:01 +0200

    Updating objects in state

--- src/learn/learn-react/adding-interactivity/updating-objects-in-state/index.js
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-export default function Index() {}
+export default function Index() {
+  return <MovingDot />;
+}
 
 function MovingDot() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
