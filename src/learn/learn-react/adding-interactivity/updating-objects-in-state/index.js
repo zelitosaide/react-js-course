0d0a7ca2b4ebd02a66397d3e1902b9f81778db47
zelitosaide <zelitosaide83@gmail.com>
2023-02-01 17:27:31 +0200

    Updating objects in state

--- src/learn/learn-react/adding-interactivity/updating-objects-in-state/index.js
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/index.js
@@ -108,6 +108,9 @@ function Form2() {
           onChange={handleEmailChange}
         />
       </label>
+      <p>
+        {person.firstName} {person.lastName} ({person.email})
+      </p>
     </>
   );
 }
