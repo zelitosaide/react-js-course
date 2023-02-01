6f7302c23e92da27113b855ee418fdea98c3042c
zelitosaide <zelitosaide83@gmail.com>
2023-02-01 21:55:29 +0200

    Updating objects in state

---
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/fix-incorrect-state-updates.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge 1 of 3: Fix incorrect state updates
+ *
+ * This form has a few bugs. Click the button that increases the score
+ * a few times. Notice that it does not increase. Then edit the first
+ * name, and notice that the score has suddenly “caught up” with your
+ * changes. Finally, edit the last name, and notice that the score has
+ * disappeared completely.
+ *
+ * Your task is to fix all of these bugs. As you fix them, explain why
+ * each of them happens.
+ */
+export default function Index() {}
