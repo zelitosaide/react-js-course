d34b5a85f9a8e1abbdaf3bcaa15933dfd4f12ddf
zelitosaide <zelitosaide83@gmail.com>
2023-02-01 08:09:52 +0200

    Queueing a Series of State Updates

--- src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/index.js
+++ src/learn/learn-react/adding-interactivity/queueing-a-series-of-state-updates/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import FixARequestCounter from "./challenges/fix-a-request-counter";
+import ImplementTheStateQueue from "./challenges/implement-the-state-queue";
 
 export default function Index() {
   return (
@@ -9,6 +10,7 @@ export default function Index() {
       <Counter3 />
       <Counter />
       <FixARequestCounter />
+      <ImplementTheStateQueue />
     </>
   );
 }
