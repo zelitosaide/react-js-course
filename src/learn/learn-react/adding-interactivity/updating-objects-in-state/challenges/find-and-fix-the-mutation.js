510b6de8708bd0f5eebf2b72e2a12dae1aec529b
zelitosaide <zelitosaide83@gmail.com>
2023-02-02 10:16:14 +0200

    Updating objects in state

--- src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/find-and-fix-the-mutation.js
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/find-and-fix-the-mutation.js
@@ -20,111 +20,11 @@ const initialPosition = {
   y: 0,
 };
 
-export default function Index() {
+export default function Index() {}
+
+function Canvas() {
   const [shape, setShape] = useState({
     color: "orange",
     position: initialPosition,
   });
-
-  function handleMove(dx, dy) {
-    setShape({
-      ...shape,
-      position: {
-        x: shape.position.x + dx,
-        y: shape.position.y + dy,
-      },
-    });
-  }
-
-  function handleColorChange(e) {
-    setShape({
-      ...shape,
-      color: e.target.value,
-    });
-  }
-
-  return (
-    <>
-      <select
-        value={shape.color}
-        onChange={handleColorChange}
-      >
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background position={initialPosition} />
-      <Box
-        color={shape.color}
-        position={shape.position}
-        onMove={handleMove}
-      >
-        Drag me!
-      </Box>
-    </>
-  );
-}
-
-function Background({ position }) {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        width: 250,
-        height: 250,
-        background: "rgba(200, 200, 0, 0.2)",
-      }}
-    ></div>
-  );
-}
-
-function Box({ children, color, position, onMove }) {
-  const [lastCoordinates, setLastCoordinates] = useState(null);
-
-  function handlePoinderDown(e) {
-    e.target.setPointerCapture(e.pointerId);
-    setLastCoordinates({
-      x: e.clientX,
-      y: e.clientY,
-    });
-  }
-
-  function handlePointerMove(e) {
-    if (lastCoordinates) {
-      setLastCoordinates({
-        x: e.clientX,
-        y: e.clientY,
-      });
-      const dx = e.clientX - lastCoordinates.x;
-      const dy = e.clientY - lastCoordinates.y;
-      onMove(dx, dy);
-    }
-  }
-
-  function handlePointerUp() {
-    setLastCoordinates(null);
-  }
-
-  return (
-    <div
-      onPointerDown={handlePoinderDown}
-      onPointerMove={handlePointerMove}
-      onPointerUp={handlePointerUp}
-      style={{
-        width: 100,
-        height: 100,
-        cursor: "grab",
-        background: color,
-        position: "absolute",
-        border: "1px solid black",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        transform: `translate(${position.x}px, ${position.y}px)`,
-      }}
-    >
-      {children}
-    </div>
-  );
 }
