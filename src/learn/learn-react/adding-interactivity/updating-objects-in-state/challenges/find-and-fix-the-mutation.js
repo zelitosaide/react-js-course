e00f15c2193f31199175147336873402cf93624d
zelitosaide <zelitosaide83@gmail.com>
2023-02-02 07:05:32 +0200

    Updating objects in state

--- src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/find-and-fix-the-mutation.js
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/find-and-fix-the-mutation.js
@@ -3,4 +3,10 @@
  *
  * There is a draggable box on a static background. You can change the
  * box’s color using the select input.
+ *
+ * But there is a bug. If you move the box first, and then change its color,
+ * the background (which isn’t supposed to move!) will “jump” to the box
+ * position. But this should not happen: the Background’s position prop is set
+ * to initialPosition, which is { x: 0, y: 0 }. Why is the background moving
+ * after the color change?
  */
