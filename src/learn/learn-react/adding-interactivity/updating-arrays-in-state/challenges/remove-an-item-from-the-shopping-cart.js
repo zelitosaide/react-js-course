979741968a6af8f72302255e893feeccd6e74af8
zelitosaide <zelitosaide83@gmail.com>
2023-02-03 17:17:19 +0200

    Updating arrays in state

--- src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/remove-an-item-from-the-shopping-cart.js
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/remove-an-item-from-the-shopping-cart.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * Challenge 2 of 4: Remove an item from the shopping cart
  */
@@ -5,4 +7,6 @@ export default function Index() {
   return <ShoppingCart />;
 }
 
-function ShoppingCart() {}
+function ShoppingCart() {
+  const [products, setProducts] = useState();
+}
