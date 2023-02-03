702c74a8356cf7c76ea2cb6412b5e21503a2a729
zelitosaide <zelitosaide83@gmail.com>
2023-02-03 17:42:01 +0200

    Updating arrays in state

---
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/fix-the-mutation-using-non-mutative-methods.js
@@ -0,0 +1,8 @@
+/**
+ * Challenge 3 of 4: Fix the mutations using non-mutative methods
+ *
+ * In this example, all of the event handlers in App.js use mutation. As a
+ * result, editing and deleting todos doesn’t work. Rewrite handleAddTodo,
+ * handleChangeTodo, and handleDeleteTodo to use the non-mutative methods:
+ */
+export default function Index() {}
