ef604174ffcae06dce182da984f81dbbe289bed1
zelitosaide <zelitosaide83@gmail.com>
2023-02-03 09:25:25 +0200

    Updating arrays in state

--- src/learn/learn-react/adding-interactivity/updating-arrays-in-state/index.js
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/index.js
@@ -3,10 +3,32 @@ import { useState } from "react";
 let nextId = 0;
 
 export default function Index() {
-  return <List />;
+  return (
+    <>
+      <List2 />
+      <List />
+    </>
+  );
 }
 
 function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input
+        value={name}
+        onChange={function (e) {
+          setName(e.target.value);
+        }}
+      />
+    </>
+  );
+}
+
+function List2() {
   const [name, setName] = useState("");
   const [artists, setArtists] = useState([]);
 
