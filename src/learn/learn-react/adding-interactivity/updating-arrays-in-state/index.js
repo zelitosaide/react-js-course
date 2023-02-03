80169988e70df684bc73f47483f667a378d4a43e
zelitosaide <zelitosaide83@gmail.com>
2023-02-03 08:45:05 +0200

    Updating arrays in state

--- src/learn/learn-react/adding-interactivity/updating-arrays-in-state/index.js
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/index.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 export default function Index() {
   return <List />;
 }
 
 function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
   return (
     <>
       <h1>Inspiring sculptors:</h1>
