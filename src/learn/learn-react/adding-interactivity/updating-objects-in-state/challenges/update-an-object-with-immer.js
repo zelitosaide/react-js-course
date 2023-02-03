0a7cf3f72f0d1d458d296cca4d23a50653f4e742
zelitosaide <zelitosaide83@gmail.com>
2023-02-03 07:21:22 +0200

    Updating objects in state

--- src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/update-an-object-with-immer.js
+++ src/learn/learn-react/adding-interactivity/updating-objects-in-state/challenges/update-an-object-with-immer.js
@@ -1,3 +1,5 @@
+import { useImmer } from "use-immer";
+
 /**
  * Challenge 3 of 3: Update an object with Immer
  *
@@ -5,4 +7,20 @@
  * fix the mutation by using Immer. For your convenience, useImmer is
  * already imported, so you need to change the shape state variable to use it.
  */
-export default function Index() {}
+
+const initialPosition = {
+  x: 0,
+  y: 0,
+};
+export default function Index() {
+  return <Canvas />;
+}
+
+function Canvas() {
+  const [shape, updateShape] = useImmer({
+    color: "orange",
+    position: initialPosition,
+  });
+}
+
+function Background() {}
