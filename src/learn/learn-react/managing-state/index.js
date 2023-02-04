b3c1191950af7509c6b98710b6420a090c923863
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 12:51:53 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -27,7 +27,13 @@ function Accordion() {
         With a population of about 2 million, Almaty is Kazakhstan's largest
         city. From 1929 to 1997, it was its capital city.
       </Panel>
-      <Panel>
+      <Panel
+        title="Etymology"
+        isActive={activeIndex === 1}
+        onShow={function () {
+          setActiveIndex(1);
+        }}
+      >
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
         "apple" and is often translated as "full of apples". In fact, the region
         surrounding Almaty is thought to be the ancestral home of the apple, and
