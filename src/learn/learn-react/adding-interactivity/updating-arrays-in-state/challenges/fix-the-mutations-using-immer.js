b1c27fa851f331a4d7630d6538272aa606a32b86
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 07:47:41 +0200

    Updating arrays in state

--- src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/fix-the-mutations-using-immer.js
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/fix-the-mutations-using-immer.js
@@ -9,7 +9,9 @@ import { todos as initialTodos } from "../../../../../data/data";
  * the mutations by using Immer. For your convenience, useImmer is already
  * imported, so you need to change the todos state variable to use it.
  */
-export default function Index() {}
+export default function Index() {
+  return <TaskApp />;
+}
 
 function TaskApp() {
   const [todos, setTodos] = useState(initialTodos);
