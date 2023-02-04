eb0fa44ba49c99f9e9640e867b0aee9224ee1b31
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 10:44:05 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -8,4 +8,8 @@ function Form() {
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
 }
