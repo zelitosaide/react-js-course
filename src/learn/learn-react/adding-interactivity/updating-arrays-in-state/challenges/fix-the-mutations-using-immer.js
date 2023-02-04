8d7dbdea538e5ad012e28269d2d0cb7e4e58aca1
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 07:43:39 +0200

    Updating arrays in state

--- src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/fix-the-mutations-using-immer.js
+++ src/learn/learn-react/adding-interactivity/updating-arrays-in-state/challenges/fix-the-mutations-using-immer.js
@@ -1,3 +1,8 @@
 /**
  * Challenge 4 of 4: Fix the mutations using Immer
+ *
+ * This is the same example as in the previous challenge. This time, fix
+ * the mutations by using Immer. For your convenience, useImmer is already
+ * imported, so you need to change the todos state variable to use it.
  */
+export default function Index() {}
