35a0f81b682a54d1b1286eb67beb0915a3cf8110
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 10:42:31 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 export default function Index() {
   return <Form />;
 }
 
-function Form() {}
+function Form() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+}
