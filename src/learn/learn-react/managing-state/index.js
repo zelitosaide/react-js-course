e10731be22e5babfde4a9352e415acbf3bbb107b
zelitosaide <zelitosaide83@gmail.com>
2023-02-04 10:53:01 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -16,6 +16,9 @@ function Form() {
   return (
     <>
       <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
     </>
   );
 }
