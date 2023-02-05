4fc4f276b43c03425d4e7bb8f48df6fefd6afd7e
zelitosaide <zelitosaide83@gmail.com>
2023-02-05 19:19:28 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -155,6 +155,11 @@ function tasksReducer(tasks, action) {
         }
       });
     }
+    case "deleted": {
+      return tasks.filter(function (t) {
+        return t.id !== action.id;
+      });
+    }
     default: {
       throw Error("Unknown action: " + action.type);
     }
