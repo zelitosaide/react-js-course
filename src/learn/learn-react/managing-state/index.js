d010c5546cb878d6ac77ba814e11a4df0a71494e
zelitosaide <zelitosaide83@gmail.com>
2023-02-05 19:02:31 +0200

    Managing State

--- src/learn/learn-react/managing-state/index.js
+++ src/learn/learn-react/managing-state/index.js
@@ -83,6 +83,19 @@ function TasksProvider({ children }) {
 
 function tasksReducer(tasks, action) {
   switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
   }
 }
 
