56f9aef18b2b5bb052617cc849d434e6e23e3515
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 18:15:42 +0200

    Reacting to input with state

---
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/challenges/add-and-remove-a-css-class.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge 1 of 3: Add and remove a CSS class
+ *
+ * Make it so that clicking on the picture removes the background--active
+ * CSS class from the outer <div>, but adds the picture--active class to
+ * the <img>. Clicking the background again should restore the original CSS
+ * classes.
+ *
+ * Visually, you should expect that clicking on the picture removes the
+ * purple background and highlights the picture border. Clicking outside
+ * the picture highlights the background, but removes the picture border highlight.
+ */
+export default function Index() {}
