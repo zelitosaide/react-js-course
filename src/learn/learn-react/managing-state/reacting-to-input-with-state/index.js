b16fa9cd476d030091d4102ff7cca5f92a8d5768
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 12:03:14 +0200

    Reacting to input with state

--- src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
@@ -16,6 +16,10 @@ function Form() {
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
 }
 
 function DisplayingManyVisualStatesAtOnce() {
