88fc18243996eba5e2ccb9f9108baaffd91ea7c1
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 19:37:48 +0200

    Reacting to input with state

--- src/learn/learn-react/managing-state/reacting-to-input-with-state/challenges/profile-editor-plain-js-and-dom.js
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/challenges/profile-editor-plain-js-and-dom.js
@@ -1,7 +1,11 @@
 function handleFormSubmit(e) {
   e.preventDefault();
+  if (editButton.textContent === "Edit Profile") {
+    editButton.textContent = "Save Profile";
+  }
 }
 
+let editButton = document.getElementById("editButton");
 let form = document.getElementById("form");
 
 form.onsubmit = handleFormSubmit;
