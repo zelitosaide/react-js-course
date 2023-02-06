ae7e198dd681268c30c1e69536f0016690b2c962
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 08:05:47 +0200

    Reacting to input with state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+function handleTextareaChange() {
+  if (textarea.value.length === 0) {
+    disable(button);
+  } else {
+    enable(button);
+  }
+}
+
+function enable(el) {
+  el.disabled = false;
+}
+
+function disable(el) {
+  el.disabled = true;
+}
+
+let textarea = document.getElementById("textarea");
+let button = document.getElementById("button");
+
+textarea.oninput = handleTextareaChange;
--- src/learn/learn-react/managing-state/reacting-to-input-with-state/imperative-example.js/index.js
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/imperative-example.js/index.js
@@ -1,18 +1,1 @@
-function handleTextareaChange() {
-  if (textarea.value.length === 0) {
-    disable(button);
-  } else {
-    enable(button);
-  }
-}
 
-function enable(el) {
-  el.disabled = false;
-}
-
-function disable(el) {
-  el.disabled = true;
-}
-
-let textarea = document.getElementById("textarea");
-let button = document.getElementById("button");
