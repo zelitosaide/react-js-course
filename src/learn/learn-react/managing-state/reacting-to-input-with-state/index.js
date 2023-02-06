f8b2496c8180bd34564248d9032ef3d154cee99a
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 10:39:01 +0200

    Reacting to input with state

--- src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
@@ -6,4 +6,13 @@ function Form({ status }) {
   if (status === "success") {
     return <h1>That's right!</h1>;
   }
+
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+    </>
+  );
 }
