ebcddb9e6f23964dd1d1bf51438d9afadd5bd09d
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 09:05:53 +0200

    Reacting to input with state

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ async function handleFormSubmit(e) {
   disable(button);
   show(loadingMessage);
   hide(errorMessage);
+  hide(form);
   try {
     await submitForm(textarea.value);
     show(successMessage);
