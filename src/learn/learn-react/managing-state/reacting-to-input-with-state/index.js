0c5866a81cc9c5b750075797b953266083cbde25
zelitosaide <zelitosaide83@gmail.com>
2023-02-06 10:37:08 +0200

    Reacting to input with state

--- src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/index.js
@@ -1,5 +1,9 @@
 export default function Index() {
-  return <Form />;
+  return <Form status="empty" />;
 }
 
-function Form({ status }) {}
+function Form({ status }) {
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
+}
