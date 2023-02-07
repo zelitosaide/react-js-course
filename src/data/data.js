c101c2c09b1d37805f46e04355d194d9ebb56338
zelitosaide <zelitosaide83@gmail.com>
2023-02-07 17:44:09 +0200

    Choosing the State Structure

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,266 @@ export const user = {
   imageSize: 90,
 };
 
+export const travelPlan = {
+  id: 0,
+  title: "(Root)",
+  childPlaces: [
+    {
+      id: 1,
+      title: "Earth",
+      childPlaces: [
+        {
+          id: 2,
+          title: "Africa",
+          childPlaces: [
+            {
+              id: 3,
+              title: "Botswana",
+              childPlaces: [],
+            },
+            {
+              id: 4,
+              title: "Egypt",
+              childPlaces: [],
+            },
+            {
+              id: 5,
+              title: "Kenya",
+              childPlaces: [],
+            },
+            {
+              id: 6,
+              title: "Madagascar",
+              childPlaces: [],
+            },
+            {
+              id: 7,
+              title: "Morocco",
+              childPlaces: [],
+            },
+            {
+              id: 8,
+              title: "Nigeria",
+              childPlaces: [],
+            },
+            {
+              id: 9,
+              title: "South Africa",
+              childPlaces: [],
+            },
+          ],
+        },
+        {
+          id: 10,
+          title: "Americas",
+          childPlaces: [
+            {
+              id: 11,
+              title: "Argentina",
+              childPlaces: [],
+            },
+            {
+              id: 12,
+              title: "Brazil",
+              childPlaces: [],
+            },
+            {
+              id: 13,
+              title: "Barbados",
+              childPlaces: [],
+            },
+            {
+              id: 14,
+              title: "Canada",
+              childPlaces: [],
+            },
+            {
+              id: 15,
+              title: "Jamaica",
+              childPlaces: [],
+            },
+            {
+              id: 16,
+              title: "Mexico",
+              childPlaces: [],
+            },
+            {
+              id: 17,
+              title: "Trinidad and Tobago",
+              childPlaces: [],
+            },
+            {
+              id: 18,
+              title: "Venezuela",
+              childPlaces: [],
+            },
+          ],
+        },
+        {
+          id: 19,
+          title: "Asia",
+          childPlaces: [
+            {
+              id: 20,
+              title: "China",
+              childPlaces: [],
+            },
+            {
+              id: 21,
+              title: "Hong Kong",
+              childPlaces: [],
+            },
+            {
+              id: 22,
+              title: "India",
+              childPlaces: [],
+            },
+            {
+              id: 23,
+              title: "Singapore",
+              childPlaces: [],
+            },
+            {
+              id: 24,
+              title: "South Korea",
+              childPlaces: [],
+            },
+            {
+              id: 25,
+              title: "Thailand",
+              childPlaces: [],
+            },
+            {
+              id: 26,
+              title: "Vietnam",
+              childPlaces: [],
+            },
+          ],
+        },
+        {
+          id: 27,
+          title: "Europe",
+          childPlaces: [
+            {
+              id: 28,
+              title: "Croatia",
+              childPlaces: [],
+            },
+            {
+              id: 29,
+              title: "France",
+              childPlaces: [],
+            },
+            {
+              id: 30,
+              title: "Germany",
+              childPlaces: [],
+            },
+            {
+              id: 31,
+              title: "Italy",
+              childPlaces: [],
+            },
+            {
+              id: 32,
+              title: "Portugal",
+              childPlaces: [],
+            },
+            {
+              id: 33,
+              title: "Spain",
+              childPlaces: [],
+            },
+            {
+              id: 34,
+              title: "Turkey",
+              childPlaces: [],
+            },
+          ],
+        },
+        {
+          id: 35,
+          title: "Oceania",
+          childPlaces: [
+            {
+              id: 36,
+              title: "Australia",
+              childPlaces: [],
+            },
+            {
+              id: 37,
+              title: "Bora Bora (French Polynesia)",
+              childPlaces: [],
+            },
+            {
+              id: 38,
+              title: "Easter Island (Chile)",
+              childPlaces: [],
+            },
+            {
+              id: 39,
+              title: "Fiji",
+              childPlaces: [],
+            },
+            {
+              id: 40,
+              title: "Hawaii (the USA)",
+              childPlaces: [],
+            },
+            {
+              id: 41,
+              title: "New Zealand",
+              childPlaces: [],
+            },
+            {
+              id: 42,
+              title: "Vanuatu",
+              childPlaces: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 43,
+      title: "Moon",
+      childPlaces: [
+        {
+          id: 44,
+          title: "Rheita",
+          childPlaces: [],
+        },
+        {
+          id: 45,
+          title: "Piccolomini",
+          childPlaces: [],
+        },
+        {
+          id: 46,
+          title: "Tycho",
+          childPlaces: [],
+        },
+      ],
+    },
+    {
+      id: 47,
+      title: "Mars",
+      childPlaces: [
+        {
+          id: 48,
+          title: "Corn Town",
+          childPlaces: [],
+        },
+        {
+          id: 49,
+          title: "Green Hill",
+          childPlaces: [],
+        },
+      ],
+    },
+  ],
+};
+
 export const menu = [
   { title: "pretzels", id: 0 },
   { title: "crispy seaweed", id: 1 },
