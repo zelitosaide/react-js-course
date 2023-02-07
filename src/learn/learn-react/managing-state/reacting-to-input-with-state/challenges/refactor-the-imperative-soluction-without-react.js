fea073af0020602806746a334d573994484e7488
zelitosaide <zelitosaide83@gmail.com>
2023-02-07 07:14:06 +0200

    Reacting to input with state

---
+++ src/learn/learn-react/managing-state/reacting-to-input-with-state/challenges/refactor-the-imperative-soluction-without-react.js
@@ -0,0 +1,11 @@
+/**
+ * Challenge 3 of 3: Refactor the imperative solution without React
+ *
+ * Imagine React didn’t exist. Can you refactor this code in a way
+ * that makes the logic less fragile and more similar to the React
+ * version? What would it look like if the state was explicit, like in React?
+ *
+ * f you’re struggling to think where to start, the stub below already
+ * has most of the structure in place. If you start here, fill in the
+ * missing logic in the updateDOM function. (Refer to the original code where needed.)
+ */
