d990004a9990cc982b4e3850fa9f3d175ad18f82
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 14:39:37 +0200

    Choosing the State Structure

--- src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-the-disappearing-selection.js
+++ src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-the-disappearing-selection.js
@@ -25,6 +25,10 @@ function MailClient() {
   const [letters, setLetters] = useState(initialLetters);
   const [highlightedId, setHighlightedId] = useState(null);
 
+  function handleHover(letterId) {
+    setHighlightedId(letterId);
+  }
+
   return (
     <>
       <h2>Inbox</h2>
@@ -34,6 +38,8 @@ function MailClient() {
             <Letter
               key={letter.id}
               letter={letter}
+              isHighlighted={letter.id === highlightedId}
+              onHover={handleHover}
             />
           );
         })}
