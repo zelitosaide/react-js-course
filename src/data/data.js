e996fed9927dee03aada833634fa5d09134999c0
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 08:16:39 +0200

    Choosing the State Structure

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,259 @@ export const user = {
   imageSize: 90,
 };
 
+export const flatTravelPlan = {
+  0: {
+    id: 0,
+    title: "(Root)",
+    childIds: [1, 43, 47],
+  },
+  1: {
+    id: 1,
+    title: "Earth",
+    childIds: [2, 10, 19, 27, 35],
+  },
+  2: {
+    id: 2,
+    title: "Africa",
+    childIds: [3, 4, 5, 6, 7, 8, 9],
+  },
+  3: {
+    id: 3,
+    title: "Botswana",
+    childIds: [],
+  },
+  4: {
+    id: 4,
+    title: "Egypt",
+    childIds: [],
+  },
+  5: {
+    id: 5,
+    title: "Kenya",
+    childIds: [],
+  },
+  6: {
+    id: 6,
+    title: "Madagascar",
+    childIds: [],
+  },
+  7: {
+    id: 7,
+    title: "Morocco",
+    childIds: [],
+  },
+  8: {
+    id: 8,
+    title: "Nigeria",
+    childIds: [],
+  },
+  9: {
+    id: 9,
+    title: "South Africa",
+    childIds: [],
+  },
+  10: {
+    id: 10,
+    title: "Americas",
+    childIds: [11, 12, 13, 14, 15, 16, 17, 18],
+  },
+  11: {
+    id: 11,
+    title: "Argentina",
+    childIds: [],
+  },
+  12: {
+    id: 12,
+    title: "Brazil",
+    childIds: [],
+  },
+  13: {
+    id: 13,
+    title: "Barbados",
+    childIds: [],
+  },
+  14: {
+    id: 14,
+    title: "Canada",
+    childIds: [],
+  },
+  15: {
+    id: 15,
+    title: "Jamaica",
+    childIds: [],
+  },
+  16: {
+    id: 16,
+    title: "Mexico",
+    childIds: [],
+  },
+  17: {
+    id: 17,
+    title: "Trinidad and Tobago",
+    childIds: [],
+  },
+  18: {
+    id: 18,
+    title: "Venezuela",
+    childIds: [],
+  },
+  19: {
+    id: 19,
+    title: "Asia",
+    childIds: [20, 21, 22, 23, 24, 25, 26],
+  },
+  20: {
+    id: 20,
+    title: "China",
+    childIds: [],
+  },
+  21: {
+    id: 21,
+    title: "Hong Kong",
+    childIds: [],
+  },
+  22: {
+    id: 22,
+    title: "India",
+    childIds: [],
+  },
+  23: {
+    id: 23,
+    title: "Singapore",
+    childIds: [],
+  },
+  24: {
+    id: 24,
+    title: "South Korea",
+    childIds: [],
+  },
+  25: {
+    id: 25,
+    title: "Thailand",
+    childIds: [],
+  },
+  26: {
+    id: 26,
+    title: "Vietnam",
+    childIds: [],
+  },
+  27: {
+    id: 27,
+    title: "Europe",
+    childIds: [28, 29, 30, 31, 32, 33, 34],
+  },
+  28: {
+    id: 28,
+    title: "Croatia",
+    childIds: [],
+  },
+  29: {
+    id: 29,
+    title: "France",
+    childIds: [],
+  },
+  30: {
+    id: 30,
+    title: "Germany",
+    childIds: [],
+  },
+  31: {
+    id: 31,
+    title: "Italy",
+    childIds: [],
+  },
+  32: {
+    id: 32,
+    title: "Portugal",
+    childIds: [],
+  },
+  33: {
+    id: 33,
+    title: "Spain",
+    childIds: [],
+  },
+  34: {
+    id: 34,
+    title: "Turkey",
+    childIds: [],
+  },
+  35: {
+    id: 35,
+    title: "Oceania",
+    childIds: [36, 37, 38, 39, 40, 41, 42],
+  },
+  36: {
+    id: 36,
+    title: "Australia",
+    childIds: [],
+  },
+  37: {
+    id: 37,
+    title: "Bora Bora (French Polynesia)",
+    childIds: [],
+  },
+  38: {
+    id: 38,
+    title: "Easter Island (Chile)",
+    childIds: [],
+  },
+  39: {
+    id: 39,
+    title: "Fiji",
+    childIds: [],
+  },
+  40: {
+    id: 40,
+    title: "Hawaii (the USA)",
+    childIds: [],
+  },
+  41: {
+    id: 41,
+    title: "New Zealand",
+    childIds: [],
+  },
+  42: {
+    id: 42,
+    title: "Vanuatu",
+    childIds: [],
+  },
+  43: {
+    id: 43,
+    title: "Moon",
+    childIds: [44, 45, 46],
+  },
+  44: {
+    id: 44,
+    title: "Rheita",
+    childIds: [],
+  },
+  45: {
+    id: 45,
+    title: "Piccolomini",
+    childIds: [],
+  },
+  46: {
+    id: 46,
+    title: "Tycho",
+    childIds: [],
+  },
+  47: {
+    id: 47,
+    title: "Mars",
+    childIds: [48, 49],
+  },
+  48: {
+    id: 48,
+    title: "Corn Town",
+    childIds: [],
+  },
+  49: {
+    id: 49,
+    title: "Green Hill",
+    childIds: [],
+  },
+};
+
 export const travelPlan = {
   id: 0,
   title: "(Root)",
