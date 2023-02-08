35259e5e0113efe1270d1b86a16295ed67dec9ba
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 11:54:03 +0200

    Choosing the State Structure

---
+++ src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-a-broken-packing-list.js
@@ -0,0 +1,10 @@
+/**
+ * Challenge 2 of 4: Fix a broken packing list
+ *
+ * This packing list has a footer that shows how many items are packed,
+ * and how many items there are overall. It seems to work at first, but
+ * it is buggy. For example, if you mark an item as packed and then delete
+ * it, the counter will not be updated correctly. Fix the counter so that
+ * it’s always correct.
+ */
+export default function Index() {}
