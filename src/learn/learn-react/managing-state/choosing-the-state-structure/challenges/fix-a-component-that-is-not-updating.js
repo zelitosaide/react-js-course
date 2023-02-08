202e0c8875ff44ce10bc829ce185b92cfedbc1b7
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 11:36:14 +0200

    Choosing the State Structure

---
+++ src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-a-component-that-is-not-updating.js
@@ -0,0 +1,9 @@
+/**
+ * Challenge 1 of 4: Fix a component that’s not updating
+ *
+ * This Clock component receives two props: color and time. When you
+ * select a different color in the select box, the Clock component
+ * receives a different color prop from its parent component. However,
+ * for some reason, the displayed color doesn’t update. Why? Fix the problem.
+ */
+export default function Index() {}
