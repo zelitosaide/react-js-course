5919decf63f0edfd6de9f8f174287e6f6921798d
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 20:37:18 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -32,6 +32,11 @@ function Panel({ title, children, isActive }) {
   return (
     <section className="panel">
       <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={function () {}}>Show</button>
+      )}
     </section>
   );
 }
