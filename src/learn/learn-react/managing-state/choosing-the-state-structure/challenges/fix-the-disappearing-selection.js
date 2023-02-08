c24051ea0e9c592d47552dba26cb1f0bdd2391d5
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 14:37:17 +0200

    Choosing the State Structure

--- src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-the-disappearing-selection.js
+++ src/learn/learn-react/managing-state/choosing-the-state-structure/challenges/fix-the-disappearing-selection.js
@@ -28,6 +28,13 @@ function MailClient() {
   return (
     <>
       <h2>Inbox</h2>
+      <ul>
+        {letters.map(function (letter) {
+          return <Letter key={letter.id} />;
+        })}
+      </ul>
     </>
   );
 }
+
+function Letter({ letter }) {}
