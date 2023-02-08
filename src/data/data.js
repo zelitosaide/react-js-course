8d9495b4e81481319924947fdaa92252e2fc375f
zelitosaide <zelitosaide83@gmail.com>
2023-02-08 13:12:21 +0200

    Choosing the State Structure

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,24 @@ export const user = {
   imageSize: 90,
 };
 
+export const letters = [
+  {
+    id: 0,
+    subject: "Ready for adventure?",
+    isStarred: true,
+  },
+  {
+    id: 1,
+    subject: "Time to check in!",
+    isStarred: false,
+  },
+  {
+    id: 2,
+    subject: "Festival Begins in Just SEVEN Days!",
+    isStarred: false,
+  },
+];
+
 export const packingItems = [
   { id: 0, title: "Warm socks", packed: true },
   { id: 1, title: "Travel journal", packed: false },
