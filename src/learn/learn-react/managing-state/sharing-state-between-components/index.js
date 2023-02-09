5b257f465eeca9467ed77b9f1e6bbbd9c2044976
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 05:36:44 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -17,11 +17,11 @@ function Accordion() {
         city. From 1929 to 1997, it was its capital city.
       </Panel>
       <Panel title="Etymology">
-        The name comes from алма, the Kazakh word for "apple" and is often
-        translated as "full of apples". In fact, the region surrounding Almaty
-        is thought to be the ancestral home of the apple, and the wild{" "}
-        <i lang="la">Malus sieversii</i> is considered a likely candidate for
-        the ancestor of the modern domestic apple.
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
+        "apple" and is often translated as "full of apples". In fact, the region
+        surrounding Almaty is thought to be the ancestral home of the apple, and
+        the wild <i lang="la">Malus sieversii</i> is considered a likely
+        candidate for the ancestor of the modern domestic apple.
       </Panel>
     </>
   );
