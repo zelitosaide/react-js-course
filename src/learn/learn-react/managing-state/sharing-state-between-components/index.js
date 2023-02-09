ce8b0b8414cc6045df03be5d82e8adaa96bb8189
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 05:52:07 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -3,31 +3,31 @@ import { useState } from "react";
 export default function Index() {
   return (
     <>
-      <Accordion />
+      <Accordion2 />
     </>
   );
 }
 
-function Accordion() {
+function Accordion2() {
   return (
     <>
       <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About">
+      <Panel2 title="About">
         With a population of about 2 million, Almaty is Kazakhstan's largest
         city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel title="Etymology">
+      </Panel2>
+      <Panel2 title="Etymology">
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
         "apple" and is often translated as "full of apples". In fact, the region
         surrounding Almaty is thought to be the ancestral home of the apple, and
         the wild <i lang="la">Malus sieversii</i> is considered a likely
         candidate for the ancestor of the modern domestic apple.
-      </Panel>
+      </Panel2>
     </>
   );
 }
 
-function Panel({ title, children }) {
+function Panel2({ title, children }) {
   const [isActive, setIsActive] = useState(false);
 
   return (
