2ac7522b05e5ffd973cadb0ba31b0dd2202c7cb4
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 06:04:46 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -36,10 +36,10 @@ function Accordion() {
 
 function Panel({ title, children, isActive }) {
   return (
-    <>
+    <section className="panel">
       <h3>{title}</h3>
       {isActive ? <p>{children}</p> : <button>Show</button>}
-    </>
+    </section>
   );
 }
 
