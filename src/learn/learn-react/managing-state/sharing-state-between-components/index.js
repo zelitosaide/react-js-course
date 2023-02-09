48dd91330a6e24496209f94187098744a4308942
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 05:33:06 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -1,5 +1,21 @@
 import { useState } from "react";
 
 export default function Index() {
-  return <></>;
+  return (
+    <>
+      <Accordion />
+    </>
+  );
 }
+
+function Accordion() {
+  return (
+    <>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About"></Panel>
+      <Panel title="Etymology"></Panel>
+    </>
+  );
+}
+
+function Panel() {}
