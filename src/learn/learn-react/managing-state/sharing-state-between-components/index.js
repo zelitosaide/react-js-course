a1ee1ce3e550c02c05be9d770b8b2e47c2ae9a3d
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 06:04:06 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/index.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/index.js
@@ -13,11 +13,17 @@ function Accordion() {
   return (
     <>
       <h2>Almaty, Kazakhstan</h2>
-      <Panel>
+      <Panel
+        title="About"
+        isActive={true}
+      >
         With a population of about 2 million, Almaty is Kazakhstan's largest
         city. From 1929 to 1997, it was its capital city.
       </Panel>
-      <Panel>
+      <Panel
+        title="Etymology"
+        isActive={true}
+      >
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
         "apple" and is often translated as "full of apples". In fact, the region
         surrounding Almaty is thought to be the ancestral home of the apple, and
