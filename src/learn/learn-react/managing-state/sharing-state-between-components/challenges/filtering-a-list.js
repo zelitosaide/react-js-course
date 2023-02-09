8974d30c21c2d301b923c97ab1a830de3e353da7
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 08:52:05 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/challenges/filtering-a-list.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/challenges/filtering-a-list.js
@@ -5,6 +5,17 @@ import { filterItems } from "../../../../../utils/utils";
 
 /**
  * Challenge 2 of 2: Filtering a list
+ *
+ * In this example, the SearchBar has its own query state that controls
+ * the text input. Its parent FilterableList component displays a List
+ * of items, but it doesn’t take the search query into account.
+ *
+ * Use the filterItems(foods, query) function to filter the list according
+ * to the search query. To test your changes, verify that typing “s” into
+ * the input filters down the list to “Sushi”, “Shish kebab”, and “Dim sum”.
+ *
+ * Note that filterItems is already implemented and imported so you don’t
+ * need to write it yourself!
  */
 export default function Index() {
   return <FilterableList />;
