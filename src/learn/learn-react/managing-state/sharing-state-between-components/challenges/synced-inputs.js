ec5ef4b913f1ee875cc925e73c790499721376bc
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 07:23:54 +0200

    Sharing State Between Components

---
+++ src/learn/learn-react/managing-state/sharing-state-between-components/challenges/synced-inputs.js
@@ -0,0 +1,7 @@
+/**
+ * Challenge 1 of 2: Synced inputs
+ *
+ * These two inputs are independent. Make them stay in sync: editing
+ * one input should update the other input with the same text, and vice versa.
+ */
+export default function Index() {}
