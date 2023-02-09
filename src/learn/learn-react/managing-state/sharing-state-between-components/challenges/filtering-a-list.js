1c44b9b10974e35f3c3989d458ff60b5434098c0
zelitosaide <zelitosaide83@gmail.com>
2023-02-09 08:16:19 +0200

    Sharing State Between Components

--- src/learn/learn-react/managing-state/sharing-state-between-components/challenges/filtering-a-list.js
+++ src/learn/learn-react/managing-state/sharing-state-between-components/challenges/filtering-a-list.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 /**
  * Challenge 2 of 2: Filtering a list
  */
 export default function Index() {}
 
-function SearchBar() {}
+function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
+  return (
+    <label>
+      Seach{" "}
+      <input
+        value={query}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
