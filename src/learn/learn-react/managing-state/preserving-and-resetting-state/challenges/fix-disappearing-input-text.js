a64113a30c8009a0fd6721b83c8292075533fdf3
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 02:21:53 +0200

    Preserving and Resetting State

--- src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/fix-disappearing-input-text.js
+++ src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/fix-disappearing-input-text.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * Challenge 1 of 5: Fix disappearing input text
  *
@@ -5,4 +7,16 @@
  * pressing the button also accidentally resets the input. Why does
  * this happen? Fix it so that pressing the button does not reset the input text.
  */
-export default function Index() {}
+export default function Index() {
+  const [showHint, setShowHint] = useState(false);
+
+  if (showHint) {
+    return (
+      <div>
+        <p>
+          <i>Your favorite city?</i>
+        </p>
+      </div>
+    );
+  }
+}
