b49c32cb1850acc86925d5273f1e25c62b48a5e9
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 09:10:15 +0200

    Extracting State Logic into a Reducer

--- src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/index.js
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/index.js
@@ -206,13 +206,28 @@ function Task({ task, onChange, onDelete }) {
 function tasksReducer(tasks, action) {
   switch (action.type) {
     case "added": {
-      return;
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
     }
     case "changed": {
-      return;
+      return tasks.map(function (t) {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
     }
     case "deleted": {
-      return;
+      return tasks.filter(function (t) {
+        return t.id !== action.id;
+      });
     }
     default: {
       throw new Error("Unknown action: " + action.type);
