c7f250de4922c6186580dee43ef6de1d44ccef73
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 03:50:51 +0200

    Preserving and Resetting State

---
+++ src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/reset-a-detail-form.js
@@ -0,0 +1,10 @@
+/**
+ * Challenge 3 of 5: Reset a detail form
+ *
+ * This is an editable contact list. You can edit the selected contact’s
+ * details and then either press “Save” to update it, or “Reset” to undo your changes.
+ *
+ * When you select a different contact (for example, Alice), the state updates but
+ * the form keeps showing the previous contact’s details. Fix it so that the form gets
+ * reset when the selected contact changes.
+ */
