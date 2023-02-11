f9cafdb75f1d8803805869f556fa58b1f8a00bef
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 02:16:13 +0200

    Preserving and Resetting State

--- src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/fix-disappearing-input-text.js
+++ src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/fix-disappearing-input-text.js
@@ -1,4 +1,8 @@
 /**
  * Challenge 1 of 5: Fix disappearing input text
+ *
+ * This example shows a message when you press the button. However,
+ * pressing the button also accidentally resets the input. Why does
+ * this happen? Fix it so that pressing the button does not reset the input text.
  */
 export default function Index() {}
