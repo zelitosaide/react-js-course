951b6c5dadacb4b958310fc72e76d2fff7dc3ead
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 04:37:40 +0200

    Preserving and Resetting State

---
+++ src/learn/learn-react/managing-state/preserving-and-resetting-state/challenges/clear-an-image-while-it-is-loading.js
@@ -0,0 +1,9 @@
+/**
+ * Challenge 4 of 5: Clear an image while it’s loading
+ *
+ * When you press “Next”, the browser starts loading the next image. However,
+ * because it’s displayed in the same <img> tag, by default you would still
+ * see the previous image until the next one loads. This may be undesirable if
+ * it’s important for the text to always match the image. Change it so that
+ * the moment you press “Next”, the previous image immediately clears.
+ */
