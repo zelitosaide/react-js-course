7a6bd0bc44af53e393eb467c7bd7028e43a7dd34
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 07:03:54 +0200

    Extracting State Logic into a Reducer

--- src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/index.js
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/index.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
+import { tasks as initialTasks } from "../../../../data/data";
+
 export default function Index() {
   return <TaskApp />;
 }
 
-function TaskApp() {}
+function TaskApp() {
+  const [tasks, setTasks] = useState(initialTasks);
+}
