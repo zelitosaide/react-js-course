2f82d2cfbbd514545cbd7b3ac494dab1049ef2fc
zelitosaide <zelitosaide83@gmail.com>
2023-02-11 04:39:53 +0200

    Preserving and Resetting State

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,37 @@ export const user = {
   imageSize: 90,
 };
 
+export const images = [
+  {
+    place: "Penang, Malaysia",
+    src: "https://i.imgur.com/FJeJR8M.jpg",
+  },
+  {
+    place: "Lisbon, Portugal",
+    src: "https://i.imgur.com/dB2LRbj.jpg",
+  },
+  {
+    place: "Bilbao, Spain",
+    src: "https://i.imgur.com/z08o2TS.jpg",
+  },
+  {
+    place: "Valparaíso, Chile",
+    src: "https://i.imgur.com/Y3utgTi.jpg",
+  },
+  {
+    place: "Schwyz, Switzerland",
+    src: "https://i.imgur.com/JBbMpWY.jpg",
+  },
+  {
+    place: "Prague, Czechia",
+    src: "https://i.imgur.com/QwUKKmF.jpg",
+  },
+  {
+    place: "Ljubljana, Slovenia",
+    src: "https://i.imgur.com/3aIiwfm.jpg",
+  },
+];
+
 export const contactsV2 = [
   { id: 0, name: "Taylor", email: "taylor@mail.com" },
   { id: 1, name: "Alice", email: "alice@mail.com" },
