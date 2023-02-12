680a6ebe7f100c0e83ef7e2fa974c49f5b29b0c9
zelitosaide <zelitosaide83@gmail.com>
2023-02-12 11:53:46 +0200

    Extracting State Logic into a Reducer

---
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/implement-useReducer-from-scratch.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+/**
+ * Challenge 4 of 4: Implement useReducer from scratch
+ *
+ * In the earlier examples, you imported the useReducer Hook from React.
+ * This time, you will implement the useReducer Hook itself! Here is a stub
+ * to get you started. It shouldn’t take more than 10 lines of code.
+ *
+ * To test your changes, try typing into the input or select a contact.
+ */
+export function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+
+  function dispatch(action) {
+    setState(reducer(state, action));
+  }
+
+  return [state, dispatch];
+}
