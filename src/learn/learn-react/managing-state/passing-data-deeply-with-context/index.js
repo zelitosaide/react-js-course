7591d4a9592beb7e81f7a1a55e231c45368d3f83
zelitosaide <zelitosaide83@gmail.com>
2023-02-13 01:38:11 +0200

    Passing Data Deeply with Context

--- src/learn/learn-react/managing-state/passing-data-deeply-with-context/index.js
+++ src/learn/learn-react/managing-state/passing-data-deeply-with-context/index.js
@@ -15,6 +15,11 @@ function Page() {
         <Heading level={2}>Heading</Heading>
         <Heading level={2}>Heading</Heading>
         <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+        </Section>
       </Section>
     </Section>
   );
