1809b21fe7db24a89cb49c4b2670f33619deed74
zelitosaide <zelitosaide83@gmail.com>
2023-02-12 09:38:41 +0200

    Extracting State Logic into a Reducer

---
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/clear-the-input-on-sending-a-message.js
@@ -0,0 +1,9 @@
+/**
+ * Challenge 2 of 4: Clear the input on sending a message
+ *
+ * Currently, pressing “Send” doesn’t do anything. Add an event handler
+ * to the “Send” button that will:
+ *
+ * 1. Show an alert with the recipient’s email and the message.
+ * 2. Clear the message input.
+ */
