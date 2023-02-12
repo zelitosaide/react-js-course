1893972f90571ada66fe182a61f75fbd91b40843
zelitosaide <zelitosaide83@gmail.com>
2023-02-12 08:50:35 +0200

    Extracting State Logic into a Reducer

--- src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/messenger-reducer.js
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/messenger-reducer.js
@@ -2,3 +2,18 @@ export const initialState = {
   selectedId: 0,
   message: "Hello",
 };
+
+export function messengerReducer(state, action) {
+  switch (action.type) {
+    case "changed_selection": {
+      return {
+        ...state,
+        selectedId: action.contactId,
+        message: "",
+      };
+    }
+    default: {
+      throw new Error("Unknown action: " + action.type);
+    }
+  }
+}
