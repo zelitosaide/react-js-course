f741a2c7375adc039712d642a099a98335b3a907
zelitosaide <zelitosaide83@gmail.com>
2023-02-12 07:15:14 +0200

    Extracting State Logic into a Reducer

---
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/dispatch-actions-from-event-handlers.js
@@ -0,0 +1,14 @@
+/**
+ * Challenge 1 of 4: Dispatch actions from event handlers
+ *
+ * Currently, the event handlers in ContactList.js and Chat.js
+ * have // TODO comments. This is why typing into the input
+ * doesn’t work, and clicking on the buttons doesn’t change the
+ * selected recipient.
+ *
+ * Replace these two // TODOs with the code to dispatch the
+ * corresponding actions. To see the expected shape and the type
+ * of the actions, check the reducer in messengerReducer.js. The
+ * reducer is already written so you won’t need to change it. You
+ * only need to dispatch the actions in ContactList.js and Chat.js.
+ */
