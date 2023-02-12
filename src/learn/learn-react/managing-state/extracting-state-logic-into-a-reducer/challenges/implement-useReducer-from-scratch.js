5d151bb1eaefcb800f1deb379aef3d2198fdf05d
zelitosaide <zelitosaide83@gmail.com>
2023-02-12 12:00:13 +0200

    Extracting State Logic into a Reducer

--- src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/implement-useReducer-from-scratch.js
+++ src/learn/learn-react/managing-state/extracting-state-logic-into-a-reducer/challenges/implement-useReducer-from-scratch.js
@@ -12,9 +12,17 @@ import { useState } from "react";
 export function useReducer(reducer, initialState) {
   const [state, setState] = useState(initialState);
 
+  // function dispatch(action) {
+  //   const nextState = reducer(state, action);
+  //   setState(nextState);
+  // }
+
+  // OR
+
   function dispatch(action) {
-    const nextState = reducer(state, action);
-    setState(nextState);
+    setState(function (s) {
+      return reducer(s, action);
+    });
   }
 
   return [state, dispatch];
