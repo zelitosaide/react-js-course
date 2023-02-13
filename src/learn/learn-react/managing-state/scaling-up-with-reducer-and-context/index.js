a8730e5b6c997d437f8a4a3090cd5ed884a30a54
zelitosaide <zelitosaide83@gmail.com>
2023-02-13 08:39:09 +0200

    Scaling Up with Reducer and Context

--- src/learn/learn-react/managing-state/scaling-up-with-reducer-and-context/index.js
+++ src/learn/learn-react/managing-state/scaling-up-with-reducer-and-context/index.js
@@ -3,9 +3,14 @@ export default function Index() {
 }
 
 function TaskApp() {
+  function handleAddTask() {}
+
   return (
     <>
       <h1>Day off in Kyoto</h1>
+      <AddTask onAddTask={handleAddTask} />
     </>
   );
 }
+
+function AddTask({ onAddTask }) {}
