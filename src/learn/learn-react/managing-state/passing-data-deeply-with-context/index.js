8f440d732ea5431c83fef17a7df94b42ebfb3fdb
zelitosaide <zelitosaide83@gmail.com>
2023-02-13 02:04:45 +0200

    Passing Data Deeply with Context

--- src/learn/learn-react/managing-state/passing-data-deeply-with-context/index.js
+++ src/learn/learn-react/managing-state/passing-data-deeply-with-context/index.js
@@ -16,6 +16,21 @@ function Page() {
   return (
     <Section>
       <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
     </Section>
   );
 }
