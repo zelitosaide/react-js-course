80a43d52fe97aa94f50747ca040480c733b9d380
zelitosaide <zelitosaide83@gmail.com>
2023-02-13 05:07:24 +0200

    Passing Data Deeply with Context

--- src/learn/learn-react/managing-state/passing-data-deeply-with-context/challenges/replace-prop-drilling-with-context.js
+++ src/learn/learn-react/managing-state/passing-data-deeply-with-context/challenges/replace-prop-drilling-with-context.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * Challenge 1 of 1: Replace prop drilling with context
  */
@@ -5,4 +7,7 @@ export default function Index() {
   return <ReplacePropDrillingWithContext />;
 }
 
-function ReplacePropDrillingWithContext() {}
+function ReplacePropDrillingWithContext() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+}
