3e65204e2fc4f2eee586f37112e1c0de7e4b60f5
zelitosaide <zelitosaide83@gmail.com>
2023-02-25 15:18:03 +0200

    Escape Hatches

--- src/learn/learn-react/escape-hatches/index.js
+++ src/learn/learn-react/escape-hatches/index.js
@@ -1,1 +1,9 @@
-export default function Index() {}
+import { useRef } from "react";
+
+export default function Index() {
+  return <Counter />;
+}
+
+function Counter() {
+  const ref = useRef();
+}
