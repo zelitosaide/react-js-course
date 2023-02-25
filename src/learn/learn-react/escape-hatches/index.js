8763cda11b2d364d4d235a03a2bef51ff04e1bc6
zelitosaide <zelitosaide83@gmail.com>
2023-02-25 16:06:52 +0200

    Escape Hatches

--- src/learn/learn-react/escape-hatches/index.js
+++ src/learn/learn-react/escape-hatches/index.js
@@ -81,3 +81,19 @@ function VideoPlayer({ isPlaying, src }) {
     />
   );
 }
+
+function Chat() {
+  return <h1>Welcome to the chat!</h1>;
+}
+
+function createConnection() {
+  // A real implementation would actually connect to the server
+  return {
+    connect() {
+      console.log("✅ Connecting...");
+    },
+    disconnect() {
+      console.log("❌ Disconnected.");
+    },
+  };
+}
