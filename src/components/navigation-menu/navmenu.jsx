12adb39608e1c0bbd2f3f6a4dcac94ecb631e445
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-15 00:29:09 +0200

    Navigation menu

--- src/components/navigation-menu/navmenu.jsx
+++ src/components/navigation-menu/navmenu.jsx
@@ -1,7 +1,7 @@
 import "./navmenu.css";
 import { CaretDownIcon } from "@radix-ui/react-icons";
 
-import {  Root, Sub, List, Item, Trigger, Content, Link, Indicator, Viewport } from "@radix-ui/react-navigation-menu";
+import {  Root, List, Item, Trigger, Content, Link, Indicator } from "@radix-ui/react-navigation-menu";
 
 export default function Navmenu() {
   return (
