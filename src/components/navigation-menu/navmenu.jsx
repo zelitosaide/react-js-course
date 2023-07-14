768c9495cfa834e629922f60f69ad0693fb87fef
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-15 01:26:49 +0200

    Navigation menu

--- src/components/navigation-menu/navmenu.jsx
+++ src/components/navigation-menu/navmenu.jsx
@@ -18,21 +18,23 @@ export default function Navmenu() {
           </Trigger>
           <div className="NavigationMenuContentWrapper">
             <Content className="NavigationMenuContent">
-              <ul className="List">
-                <li style={{ gridRow: 'span 3' }}>
-                  <Link asChild>
-                    <a className="Callout" href="/">
-                      <svg aria-hidden width="38" height="38" viewBox="0 0 25 25" fill="white">
-                        <path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
-                        <path d="M12 0H4V8H12V0Z"></path>
-                        <path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
-                      </svg>
-                      <div className="CalloutHeading">Radix Primitives</div>
-                      <p className="CalloutText">Unstyled, accessible components for React.</p>
-                    </a>
-                  </Link>
-                </li>
-              </ul>
+              <div className="NavigationMenuContentBoundary">
+                <ul className="List">
+                  <li style={{ gridRow: 'span 3' }}>
+                    <Link asChild>
+                      <a className="Callout" href="/">
+                        <svg aria-hidden width="38" height="38" viewBox="0 0 25 25" fill="white">
+                          <path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
+                          <path d="M12 0H4V8H12V0Z"></path>
+                          <path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
+                        </svg>
+                        <div className="CalloutHeading">Radix Primitives</div>
+                        <p className="CalloutText">Unstyled, accessible components for React.</p>
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </Content>
           </div>
         </Item>
