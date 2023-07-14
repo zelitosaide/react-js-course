8fb1ef787c5e3bbdaddc42375f641b5f5a01afcd
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-14 12:15:52 +0200

    Popover

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import "./App.css";
+import * as Popover from "@radix-ui/react-popover";
+
+function PopoverDemo() {
+  return (
+    <Popover.Root>
+      <Popover.Trigger>More info</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content>
+          Some more info...
+          <Popover.Arrow />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PopoverDemo />
     </>
   )
 }
 
-export default App
+export default App;
