56ff05e4f219c739c20e91f418119d5087b23708
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-14 12:19:54 +0200

    Popover

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import * as Popover from "@radix-ui/react-popover";
 function PopoverDemo() {
   return (
     <Popover.Root>
-      <Popover.Trigger>More info</Popover.Trigger>
+      <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
       <Popover.Portal>
         <Popover.Content>
           Some more info...
