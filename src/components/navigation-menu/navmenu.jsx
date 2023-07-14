81f6925f4a92b7f00a921dc66839a7e2450067e6
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-14 17:32:32 +0200

    Navigation menu

--- src/components/navigation-menu/navmenu.jsx
+++ src/components/navigation-menu/navmenu.jsx
@@ -1,5 +1,34 @@
 import "./navmenu.css";
 
+import { 
+  Root,
+  Sub,
+  List,
+  Item,
+  Trigger,
+  Content,
+  Link,
+  Indicator,
+  Viewport
+} from "@radix-ui/react-navigation-menu";
+
 export default function Navmenu() {
-  return;
+  return (
+    <Root>
+      <List>
+        <Item>
+          <Trigger>Item one</Trigger>
+          <Content>Item one content</Content>
+        </Item>
+        <Item>
+          <Trigger>Item two</Trigger>
+          <Content>Item Two content</Content>
+        </Item>
+        <Item>
+          <Trigger>Item three</Trigger>
+          <Content>Item Three content</Content>
+        </Item>
+      </List>
+    </Root>
+  );
 }
