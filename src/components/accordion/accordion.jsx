4a10b0468c8bc094af283bd85b1fbf1c30a28551
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-14 14:13:57 +0200

    Accordion

--- src/components/accordion/accordion.jsx
+++ src/components/accordion/accordion.jsx
@@ -1,5 +1,22 @@
 import "./accordion.css";
 
+import { Root, Item, Header, Trigger, Content } from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
 export default function Component() {
-  return;
+  return (
+    <Root>
+      <Item>
+        <Header>
+          <Trigger>
+            Is it accessible?
+            <ChevronDownIcon aria-hidden />
+          </Trigger>
+        </Header>
+        <Content>
+          <div>Yes. It adheres to the WAI-ARIA design pattern.</div>
+        </Content>
+      </Item>
+    </Root>
+  );
 }
