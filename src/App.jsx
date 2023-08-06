db890dbf4f213044ca0e7331da2759e137650677
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-06 14:11:14 +0200

    useTransition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import "./App.css";
+import { useState, useTransition } from "react";
 
 import Popover from "./components/popover/popover";
 import Accordion from "./components/accordion/accordion";
@@ -6,12 +7,22 @@ import Menubar from "./components/menubar/menubar";
 import Navmenu from "./components/navigation-menu/navmenu";
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState("about");
+
+  function selectTab(nextTab) {
+    startTransition(function() {
+      setTab(nextTab);
+    });
+  }
+
   return (
     <>
       {/* <Popover trigger="More info" content="Some more info..." /> */}
       {/* <Accordion /> */}
       {/* <Menubar /> */}
-      <Navmenu />
+      {/* <Navmenu /> */}
+      <h1>Heading 1</h1>
     </>
   );
 }
