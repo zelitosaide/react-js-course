9b2e6c94a8af90204f1c63eda4428aa7478bffd3
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-10 19:45:17 +0200

    Dropdown Menu

--- src/components/dropdown-menu/dropdown-menu.jsx
+++ src/components/dropdown-menu/dropdown-menu.jsx
@@ -1,3 +1,14 @@
+import { Root, Trigger } from "@radix-ui/react-dropdown-menu";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
 export default function DropdownMenu() {
-  
+  return (
+    <Root>
+      <Trigger asChild>
+        <button>
+          <HamburgerMenuIcon />
+        </button>
+      </Trigger>
+    </Root>
+  );
 }
