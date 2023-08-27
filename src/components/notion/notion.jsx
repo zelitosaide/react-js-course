49c1056a1dcdcb5a3e00a3160ef7d3a6cd0ba3aa
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-27 10:24:38 +0200

    Notion

--- src/components/notion/notion.jsx
+++ src/components/notion/notion.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 import "./notion.css";
 
 export default function Notion() {
+  const [title, setTitle] = useState(0);
+
+  useEffect(function() {}, [title]);
+  
   return (
     <h1 className="button">Notion</h1>
   );
