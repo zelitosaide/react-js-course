9179bce22e02951494276c5097e981320ea3a6ee
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-27 17:09:59 +0200

    TipTap

--- src/components/notion/tiptap.jsx
+++ src/components/notion/tiptap.jsx
@@ -9,11 +9,27 @@ import {
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+
+import { Color } from "@tiptap/extension-color";
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+
 import { content } from "./content";
 
 const extensions = [
-  StarterKit
-];
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  TextStyle.configure({ types: [ListItem.name] }),
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+]
 
 export default function TipTap() {
   const editor = useEditor({
