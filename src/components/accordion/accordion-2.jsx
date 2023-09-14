4b20c1c1faf5771c93f1f04438362e8f0aed6873
Zelito A. Saide <zelitosaide83@gmail.com>
2023-09-14 23:07:47 +0200

    Accordion

--- src/components/accordion/accordion-2.jsx
+++ src/components/accordion/accordion-2.jsx
@@ -9,9 +9,15 @@ export default function AccordionTwo() {
     <Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible>
       <Item className="AccordionItem" value="item-1">
         <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
       </Item>
       <Item className="AccordionItem" value="item-2">
         <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's unstyled by default, giving you freedom over the look and feel.
+        </AccordionContent>
       </Item>
       <Item className="AccordionItem" value="item-3">
         <AccordionTrigger>Can it be animated?</AccordionTrigger>
