af524c8a9bbe7311227116eb3c1962d068f53a1f
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-01 19:46:55 +0200

    Dialog

--- src/components/dialog/dialog.jsx
+++ src/components/dialog/dialog.jsx
@@ -1,7 +1,34 @@
+import "./dialog.css";
+
+import { Content, Description, Overlay, Portal, Root, Title, Trigger } from "@radix-ui/react-dialog";
+
 export default function Dialog() {
   return (
-    <div>
-      Dialog
-    </div>
+    <Root>
+      <Trigger asChild>
+        <button className="Button violet">Edit profile</button>
+      </Trigger>
+      <Portal>
+        <Overlay className="DialogOverlay" />
+        <Content className="DialogContent">
+          <Title className="DialogTitle">Edit profile</Title>
+          <Description className="DialogDescription">
+            Make changes to your profile here. Click save when you're done.
+          </Description>
+          <fieldset className="Fieldset">
+            <label className="Label" htmlFor="name">
+              Name
+            </label>
+            <input className="Input" id="name" defaultValue="Pedro Duarte" />
+          </fieldset>
+          <fieldset className="Fieldset">
+            <label className="Label" htmlFor="username">
+              Username
+            </label>
+            <input className="Input" id="username" defaultValue="@peduarte" />
+          </fieldset>
+        </Content>
+      </Portal>
+    </Root>
   );
 }
